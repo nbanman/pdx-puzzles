8569ac616342f05576f5a8962f2522f0da37583a
neil <neil.banman@gmail.com>
2024-12-02 12:51:01 -0800

    added Input, Output

--- rust/advent/src/bin/2024/y24d01.rs
+++ rust/advent/src/bin/2024/y24d01.rs
@@ -4,17 +4,20 @@ use advent::utilities::get_input::get_input;
 use itertools::{Either, Itertools};
 use utilities::{parsing::get_numbers::ContainsNumbers, structs::stopwatch::{ReportDuration, Stopwatch}};
 
+type Input = (Vec<usize>, Vec<usize>);
+type Output = usize;
+
 fn main() {
     let mut stopwatch = Stopwatch::new();
     stopwatch.start();
-    let input = get_input(24, 1).unwrap();
+    let input = parse_input(&get_input(24, 1).unwrap());
     println!("Inputs loaded ({})", stopwatch.lap().report());
-    println!("1. {} ({})", part1(&input), stopwatch.lap().report());
-    println!("2. {} ({})", part2(&input), stopwatch.lap().report());
+    println!("1. {} ({})", part1(input.clone()), stopwatch.lap().report());
+    println!("2. {} ({})", part2(input), stopwatch.lap().report());
     println!("Total: {}", stopwatch.stop().report());
 }
 
-fn parse_input(input: &str) -> (Vec<usize>, Vec<usize>) {
+fn parse_input(input: &str) -> Input {
     input.get_numbers::<usize>().enumerate().partition_map(|(idx, n)| {
         if idx & 1 == 0 {
             Either::Left(n)
@@ -24,8 +27,8 @@ fn parse_input(input: &str) -> (Vec<usize>, Vec<usize>) {
     })
 }
 
-fn part1(input: &str) -> usize {
-    let (mut a, mut b) = parse_input(input);
+fn part1(input: Input) -> Output {
+    let (mut a, mut b) = input;
     a.sort_unstable();
     b.sort_unstable();
     a.into_iter().zip(b.into_iter())
@@ -33,8 +36,8 @@ fn part1(input: &str) -> usize {
         .sum()
 }
 
-fn part2(input: &str) -> usize {
-    let (a, b) = parse_input(input);
+fn part2(input: Input) -> Output {
+    let (a, b) = input;
     let mut freq = HashMap::new();
     for n in b {
         freq.entry(n)
@@ -46,9 +49,24 @@ fn part2(input: &str) -> usize {
 
 #[test]
 fn default() {
-    let input = get_input(24, 1).unwrap();
-    assert_eq!(1222801, part1(&input));
-    assert_eq!(22545250, part2(&input));
+    let input = parse_input(&get_input(24, 1).unwrap());
+    assert_eq!(1222801, part1(input.clone()));
+    assert_eq!(22545250, part2(input));
+}
+
+
+#[test]
+fn examples() {
+    let inputs = [r"3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+", ];
+    let input = parse_input(&inputs[0]);
+    assert_eq!(11, part1(input.clone()));
+    assert_eq!(31, part2(input));
 }
 
 // Inputs loaded (21μs)
