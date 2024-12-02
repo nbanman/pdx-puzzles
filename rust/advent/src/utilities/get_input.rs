1e58c41861e43164347040f8f055ff0077f3e3b0
neil <neil.banman@gmail.com>
2024-12-02 01:47:58 -0800

    initial

---
+++ rust/advent/src/utilities/get_input.rs
@@ -0,0 +1,48 @@
+use std::{fs::File, io::{self, Read}, path::Path};
+
+pub fn get_input(year: u8, day: u8) -> io::Result<String> {
+    let path = format!("./advent/inputs/20{}/y{}d{}.txt", year, year, day);
+    // Try to open local file first
+    match File::open(&path) {
+        Ok(mut file) => {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents)?;
+            Ok(contents)
+        }
+        Err(_) => {
+            // If local file doesn't exist, attempt to download
+            download_input(year, day, &path)
+        }
+    }
+}
+
+fn download_input(year: u8, day: u8, path: &str) -> Result<String, io::Error> {
+    // Ensure the directory exists
+    if let Some(parent) = Path::new(path).parent() {
+        std::fs::create_dir_all(parent)?;
+    }
+
+    let url = format!("https://adventofcode.com/20{}/day/{}/input", year, day);
+    
+    let session = std::env::var("ADVENT_SESSION")
+        .map_err(|_| io::Error::new(io::ErrorKind::NotFound, "ADVENT_SESSION environment variable not set"))?;
+    let session = format!("session={}", session);
+
+    // Propagate network and file errors
+    let mut response = ureq::get(&url)
+        .set("Cookie", &session)
+        .set("User-Agent", "github.com/nbanman/pdx-puzzles/tree/main/rust/advent/utilities/get_input.rs")
+        .call()
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?
+        .into_reader();
+
+    // Create file and copy contents
+    let mut file = File::create(path)?;
+    std::io::copy(&mut response, &mut file)?;
+
+    // Read the newly downloaded file
+    let mut contents = String::new();
+    File::open(path)?.read_to_string(&mut contents)?;
+    
+    Ok(contents)
+}
