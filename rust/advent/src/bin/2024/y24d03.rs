bf55496996af52b82f68ac8f89fd07202806e3d0
neil <neil.banman@gmail.com>
2024-12-02 22:19:23 -0800

    day 3

--- rust/advent/src/bin/2024/y24d03.rs
+++ rust/advent/src/bin/2024/y24d03.rs
@@ -1,44 +1,64 @@
 use advent::utilities::get_input::get_input;
-use utilities::structs::stopwatch::{ReportDuration, Stopwatch};
+use lazy_regex::Regex;
+use utilities::{parsing::get_numbers::ContainsNumbers, structs::stopwatch::{ReportDuration, Stopwatch}};
+use std::ops::Mul;
 
-type Input = usize;
+type Input<'a> = &'a str;
 type Output = usize;
 
 fn main() {
     let mut stopwatch = Stopwatch::new();
     stopwatch.start();
     let input = get_input(24, 3).unwrap();
-    let input = parse_input(&input);
     println!("Input parsed ({})", stopwatch.lap().report());
     println!("1. {} ({})", part1(&input), stopwatch.lap().report());
     println!("2. {} ({})", part2(&input), stopwatch.lap().report());
     println!("Total: {}", stopwatch.stop().report());
 }
 
-fn parse_input(input: &str) -> Input {
-    todo!()
+fn part1(input: Input) -> Output {
+    let rx = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+    rx.find_iter(input)
+        .map(|rx_match| rx_match.as_str().get_numbers().reduce(usize::mul).unwrap())
+        .sum()
 }
 
-fn part1(input: &Input) -> Output {
-    todo!()
-}
-
-fn part2(input: &Input) -> Output {
-    todo!()
+fn part2(input: Input) -> Output {
+    let rx = Regex::new(r"mul\(\d+,\d+\)|do\(\)|don't\(\)").unwrap();
+    rx.find_iter(input)
+        .map(|rx_match| rx_match.as_str())
+        .fold((0, true), |(sum, active), instruction| {
+            let new_active = match instruction {
+                "do()" => true,
+                "don't()" => false,
+                _ => active,
+            };
+            let new_sum = sum + if active && instruction.starts_with("mul") {
+                instruction.get_numbers().reduce(usize::mul).unwrap()
+            } else {
+                0
+            };
+            (new_sum, new_active)
+        })
+        .0
 }
 
 #[test]
 fn default() {
-    let input = get_input(24, 2).unwrap();
-    let input = parse_input(&input);
-    assert_eq!(X, part1(&input));
-    assert_eq!(X, part2(&input));
+    let input = get_input(24, 3).unwrap();
+    assert_eq!(191183308, part1(&input));
+    assert_eq!(92082041, part2(&input));
 }
 
 #[test]
 fn examples() {
-    let inputs = [r"", ];
-    let input = parse_input(&inputs[0]);
-    assert_eq!(X, part1(&input));
-    assert_eq!(X, part2(&input));
+    let inputs = [r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))", 
+    r"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",];
+    assert_eq!(161, part1(inputs[0]));
+    assert_eq!(48, part2(inputs[1]));
 }
+
+// Input parsed (37μs)
+// 1. 191183308 (513μs)
+// 2. 92082041 (401μs)
+// Total: 956μs
