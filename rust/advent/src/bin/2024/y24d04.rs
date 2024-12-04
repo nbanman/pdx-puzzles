b89443873fbe6bd29dd8bc5fa31d36fc1ff8dc67
neil <neil.banman@gmail.com>
2024-12-04 00:15:41 -0800

    inital

--- rust/advent/src/bin/2024/y24d04.rs
+++ rust/advent/src/bin/2024/y24d04.rs
@@ -19,7 +19,6 @@ fn main() {
 fn part1(input: Input) -> Output {
     let width = (input.find('\n').unwrap() + 1) as isize;
     let jumble = input.as_bytes();
-    let xmas: HashSet<u8> = "MAS".as_bytes().into_iter().cloned().collect();
     let dirs = [-width - 1, -width, -width + 1, -1, 1, width - 1, width, width + 1];
     let starts: Vec<isize> = jumble.iter().enumerate()
         .filter(|(_, &c)| c == b'X')
@@ -28,35 +27,70 @@ fn part1(input: Input) -> Output {
     starts.iter()
         .flat_map(|start| {
             dirs.iter().map(|&dir| {
-                (0..4)
+                (0..3)
                     .scan(*start, |state, _| {
                         *state += dir;
                         Some(*state)
                     })
-                    .map(|pos| *jumble.try_get(pos).unwrap())
-                    .collect::<HashSet<u8>>()
+                    .filter_map(|pos| jumble.try_get(pos))
+                    .map(|c| *c as char)
+                    .collect::<String>()
             })
         })
-        .filter(|word| xmas == *word)
+        .filter(|word| "MAS" == word)
         .count()
 }
 
 fn part2(input: Input) -> Output {
-    3
+    let width = (input.find('\n').unwrap() + 1) as isize;
+    let jumble = input.as_bytes();
+    let starts: Vec<isize> = jumble.iter().enumerate()
+        .filter(|(_, &c)| c == b'A')
+        .map(|(idx, _)| idx as isize)
+        .collect();
+    let ms: HashSet<u8> = "MS".as_bytes().iter().copied().collect();
+    let lr = [-width - 1, width + 1];
+    let rl = [-width + 1, width - 1];
+    starts.iter()
+        .filter(|&start| {
+            let lr: HashSet<u8> = lr.iter()
+                .filter_map(|pos| jumble.try_get(start + pos))
+                .copied()
+                .collect();
+            let rl: HashSet<u8> = rl.iter()
+                .filter_map(|pos| jumble.try_get(start + pos))
+                .copied()
+                .collect();
+            lr == ms && rl == ms
+        })
+        .count()
 }
 
 #[test]
 fn default() {
-    let input = get_input(24, X).unwrap();
-    let input = parse_input(&input);
-    assert_eq!(X, part1(&input));
-    // assert_eq!(X, part2(&input));
+    let input = get_input(24, 4).unwrap();
+    assert_eq!(2534, part1(&input));
+    assert_eq!(1866, part2(&input));
 }
 
 #[test]
 fn examples() {
-    let inputs = [r"", ];
-    let input = parse_input(&inputs[0]);
-    assert_eq!(X, part1(&input));
-    // assert_eq!(X, part2(&input));
+    let inputs = [r"MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+", ];
+    assert_eq!(18, part1(inputs[0]));
+    assert_eq!(9, part2(inputs[0]));
 }
+
+// Input parsed (27μs)
+// 1. 2534 (1ms)
+// 2. 1866 (701μs)
+// Total: 1ms
