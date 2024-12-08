ad28768af610d48e750f679fd5e378a597b417d8
neil <neil.banman@gmail.com>
2024-12-07 18:16:22 -0800

    removed useless functions

--- kotlin/utilities/src/main/kotlin/org/gristle/pdxpuzzles/utilities/iteration/coroutines.kt
+++ kotlin/utilities/src/main/kotlin/org/gristle/pdxpuzzles/utilities/iteration/coroutines.kt
@@ -4,16 +4,8 @@ package org.gristle.pdxpuzzles.utilities.iteration
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.withContext
 
 suspend fun <T, R> Iterable<T>.parMap(transform: (T) -> R): List<R> =
-    withContext(Dispatchers.Default) { async { map(transform) }.await() }
-
-suspend fun <T, R> Sequence<T>.parMap(transform: (T) -> R): Sequence<R> =
-    withContext(Dispatchers.Default) { async { map(transform) }.await() }
-
-suspend inline fun <T> Iterable<T>.parFilter(crossinline predicate: (T) -> Boolean): List<T> =
-    withContext(Dispatchers.Default) { async { filter(predicate) }.await() }
-
-suspend fun <T> Sequence<T>.parFilter(predicate: (T) -> Boolean): Sequence<T> =
-    withContext(Dispatchers.Default) { async { filter(predicate) }.await() }
+    withContext(Dispatchers.Default) { map { async { transform(it) } }.awaitAll() }
