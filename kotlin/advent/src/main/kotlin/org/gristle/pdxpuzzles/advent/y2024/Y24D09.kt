27ef630705102c5098ee8874742fb0d7ebc253be
neil <neil.banman@gmail.com>
2024-12-09 17:41:22 -0800

    don't move any lists, per Todd Ginsberg

--- kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2024/Y24D09.kt
+++ kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2024/Y24D09.kt
@@ -16,19 +16,20 @@ class Y24D09(val input: String) : Day {
         var left = 0
         var right = fragmented.lastIndex
         val blocks = fragmented.count { it != null }
-        val defragged = buildList {
-            while (left < blocks) {
-                while (fragmented[left] != null && left < right) add(fragmented[left++]!!)
-                while (fragmented[right] == null) right--
-                left++
-                add(fragmented[right--]!!)
-            }
+        var checksum = 0L
+
+        while (left < blocks) {
+            while (fragmented[left] != null && left < right) checksum += left * fragmented[left++]!!
+            while (fragmented[right] == null) right--
+            checksum += left * fragmented[right--]!!
+            left++
         }
-        return defragged.foldIndexed(0L) { index, acc, i -> acc + index * i }
+        return checksum
     }
 
-    private class Block(var index: Int, val size: Int, val value: Int) {
-        fun checksum(): Long = (index.toLong() until (index + size).toLong())
+    private class Block(val initialIndex: Int, val size: Int, val value: Int) {
+        fun checksum(): Long = checksum(initialIndex)
+        fun checksum(index: Int): Long = (index.toLong() until (index + size).toLong())
             .sumOf { it * value }
     }
 
@@ -47,27 +48,33 @@ class Y24D09(val input: String) : Day {
                 }
             }
         }
+        var checksum = 0L
         for (block in blocks.reversed()) {
-            val (spaceIndex, heapOffset) = spaces.subList(block.size, spaces.size)
+            val indexes = spaces.subList(block.size, spaces.size)
                 .withIndex()
                 .mapNotNull { (heapIndex, space) ->
                     space.peek()?.let { spaceIndex ->
-                        if (spaceIndex < block.index) {
+                        if (spaceIndex < block.initialIndex) {
                             spaceIndex to heapIndex
                         } else {
                             null
                         }
                     }
                 }.minByOrNull { it.first }
-                ?: continue
+
+            if (indexes == null) {
+                checksum += block.checksum()
+                continue
+            }
+            val (spaceIndex, heapOffset) = indexes
             val heapIndex = heapOffset + block.size
             spaces[heapIndex].poll()
-            block.index = spaceIndex
             if (block.size < heapIndex) {
                 spaces[heapIndex - block.size].add(spaceIndex + block.size)
             }
+            checksum += block.checksum(spaceIndex)
         }
-        return blocks.sumOf { it.checksum() }
+        return checksum
     }
 }
 
@@ -75,7 +82,7 @@ fun main() = Day.runDay(Y24D09::class)
 @Suppress("unused")
 private val test = listOf("12345", "2333133121414131402")
 
-//    Class creation: 2ms
-//    Part 1: 6390180901651 (26ms)
-//    Part 2: 6412390114238 (172ms)
-//    Total time: 201ms
+//    Class creation: 1ms
+//    Part 1: 6390180901651 (18ms)
+//    Part 2: 6412390114238 (34ms)
+//    Total time: 55ms
