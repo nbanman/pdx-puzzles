ff5ef91b9b06e160b619ab0aca1da9d84bb3a95a
neil <neil.banman@gmail.com>
2024-12-10 10:50:36 -0800

    cleanup, add comments

--- rust/advent/src/bin/2024/y24d10.rs
+++ rust/advent/src/bin/2024/y24d10.rs
@@ -20,7 +20,10 @@ fn part1(topo_map: Input) -> Output { solve(topo_map, false) }
 fn part2(topo_map: Input) -> Output { solve(topo_map, true) }
 
 fn solve(topo_map: Input, distinct_paths: bool) -> Output {
+    // working directly with string, so need width of each line to navigate in 2D properly
     let width = (topo_map.find('\n').unwrap() + 1) as isize;
+
+    // finds all trailheads, then passes that on to the paths function, then sums the result
     topo_map.as_bytes().iter().enumerate()
         .filter(|&(_, height)| *height == b'0')
         .map(|(trailhead, _)| {
@@ -29,9 +32,12 @@ fn solve(topo_map: Input, distinct_paths: bool) -> Output {
         .sum()
 }
 
+struct State { pos: usize, height: u8, }
+
+// Standard BFS, with node tracking enabled or disabled depending on which part.
 fn paths(trailhead: usize, topo_map: &[u8], width: isize, distinct_paths: bool) -> usize {
     let mut q = VecDeque::new();
-    q.push_back((trailhead, b'0'));
+    q.push_back(State { pos: trailhead, height: b'0' });
     let mut paths = 0;
     let mut visited = if distinct_paths {
         None
@@ -39,12 +45,12 @@ fn paths(trailhead: usize, topo_map: &[u8], width: isize, distinct_paths: bool) 
         Some(vec![false; topo_map.len()])
     };
     while let Some(state) = q.pop_front() {
-        if state.1 == b'9' { paths += 1; }
-        for neighbor in hike(state, topo_map, width) {
+        if state.height == b'9' { paths += 1; }
+        for neighbor in hike_path(state, topo_map, width) {
             if !distinct_paths {
                 let visited = visited.as_mut()
                     .unwrap()
-                    .get_mut(neighbor.0)
+                    .get_mut(neighbor.pos)
                     .unwrap();
                 if *visited { continue; } else { *visited = true; }
             }
@@ -54,17 +60,18 @@ fn paths(trailhead: usize, topo_map: &[u8], width: isize, distinct_paths: bool) 
     paths
 }
 
-fn hike(
-    (pos, height): (usize, u8), 
+// finds adjacent spots along path, and returns the ones that are one height higher than current state
+fn hike_path(
+    state: State, 
     topo_map: &[u8], 
     width: isize
-) -> impl Iterator<Item = (usize, u8)> + use<'_> {
+) -> impl Iterator<Item = State> + use<'_> {
     [-width, 1, width, -1].into_iter()
         .filter_map(move |offset| {
-            let neighbor_pos = pos as isize + offset;
+            let neighbor_pos = state.pos as isize + offset;
             let neighbor_height = topo_map.try_get(neighbor_pos)?;
-            if *neighbor_height == height + 1 {
-                Some((neighbor_pos as usize, *neighbor_height))
+            if *neighbor_height == state.height + 1 {
+                Some(State { pos: neighbor_pos as usize, height: *neighbor_height })
             } else {
                 None   
             }
