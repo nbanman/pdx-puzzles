99855071cb54ebbd124485014765aa0f18f59085
neil <neil.banman@gmail.com>
2024-12-10 10:49:49 -0800

    move to String based solution, refactor to cleaner version with more reused code, 2x speedup

--- kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2024/Y24D10.kt
+++ kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2024/Y24D10.kt
@@ -1,51 +1,66 @@
 package org.gristle.pdxpuzzles.advent.y2024
 
 import org.gristle.pdxpuzzles.advent.utilities.Day
-import org.gristle.pdxpuzzles.utilities.graph.Graph
-import org.gristle.pdxpuzzles.utilities.objects.Coord
-import org.gristle.pdxpuzzles.utilities.objects.toGrid
 import java.util.ArrayDeque
 
-class Y24D10(input: String) : Day {
-    private val topoMap = input.toGrid(Char::digitToInt)
-    private val trailheads = topoMap.withIndex().filter { (_, n) -> n == 0 }.map { (idx) -> topoMap.coordOf(idx) }
-    private val hike: (Pair<Coord, Int>) -> List<Pair<Coord, Int>> = { (pos, height) ->
-        topoMap.getNeighborsIndexedValue(pos)
-            .filter { (_, neighborHeight) -> height + 1 == neighborHeight }
-            .map { (neighborIndex, neighborHeight) -> topoMap.coordOf(neighborIndex) to neighborHeight }
+class Y24D10(private val topoMap: String) : Day {
+    private fun solve(distinctPaths: Boolean): Int {
+        // working directly with string, so need width of each line to navigate in 2D properly
+        val width = topoMap.indexOf('\n') + 1
+
+        // finds all trailheads, then passes that on to the paths function, then sums the result
+        return topoMap.withIndex()
+            .filter { (_, n) -> n == '0' }
+            .sumOf { (trailhead) -> paths(trailhead, width, distinctPaths) }
     }
 
-    private fun distinctPaths(trailhead: Coord): Int {
-        val q = ArrayDeque<Pair<Coord, Int>>()
-        q.add(trailhead to 0)
-        var distinctPaths = 0
+    private data class State(val pos: Int, val height: Char)
+
+    // Standard BFS, with node tracking enabled or disabled depending on which part.
+    private fun paths(trailhead: Int, width: Int, distinctPaths: Boolean): Int {
+        val q = ArrayDeque<State>()
+        q.add(State(trailhead, '0'))
+        var paths = 0
+        val neighbors = listOf(-width, 1, width, -1)
+        val visited = if (distinctPaths) null else BooleanArray(topoMap.length)
         while (q.isNotEmpty()) {
             val state = q.removeFirst()
-            if (state.second == 9) distinctPaths++
-            q.addAll(hike(state))
+            if (state.height == '9') paths++
+            for (adjacent in hikePath(state, neighbors)) {
+                if (visited != null) {
+                    if (visited[adjacent.pos]) continue else visited[adjacent.pos] = true
+                }
+                q.addLast(adjacent)
+            }
         }
-        return distinctPaths
+        return paths
     }
 
-    override fun part1(): Int = trailheads.sumOf { trailhead ->
-//        println("trailhead: $trailhead")
-        Graph.bfsSequence(trailhead to 0, defaultEdges = hike).count {
-//            if (it.id.second == 9) {
-//                println(it.id)
-//            }
-            it.id.second == 9
+    // finds adjacent spots along path, and returns the ones that are one height higher than current state
+    private fun hikePath(state: State, neighbors: List<Int>): List<State> {
+        val (pos, height) = state
+        return neighbors.mapNotNull { offset ->
+            val neighborPos = pos + offset
+            topoMap.getOrNull(neighborPos)?.let { neighborHeight ->
+                if (neighborHeight == height + 1) {
+                    State(neighborPos, neighborHeight)
+                } else {
+                    null
+                }
+            }
         }
     }
 
-    override fun part2(): Int = trailheads.sumOf(::distinctPaths)
+    override fun part1(): Int = solve(distinctPaths = false)
+    override fun part2(): Int = solve(distinctPaths = true)
 }
 
 fun main() = Day.runDay(Y24D10::class)
 
-//    Class creation: 5ms
-//    Part 1: 461 (19ms)
-//    Part 2: 875 (8ms)
-//    Total time: 33ms
+//    Class creation: 1ms
+//    Part 1: 461 (10ms)
+//    Part 2: 875 (5ms)
+//    Total time: 17ms
 
 @Suppress("unused") // 81, 227
 private val test = listOf("""0123
