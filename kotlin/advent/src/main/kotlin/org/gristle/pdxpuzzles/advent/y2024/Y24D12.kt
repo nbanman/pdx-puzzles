b177b1318b0960f4cc6a7789398732517814ce34
neil <neil.banman@gmail.com>
2024-12-12 09:29:42 -0800

    added comments

--- kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2024/Y24D12.kt
+++ kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2024/Y24D12.kt
@@ -6,7 +6,6 @@ import org.gristle.pdxpuzzles.utilities.objects.Coord
 import org.gristle.pdxpuzzles.utilities.objects.Grid
 import org.gristle.pdxpuzzles.utilities.objects.toCoord
 import org.gristle.pdxpuzzles.utilities.objects.toGrid
-import java.util.ArrayDeque
 import java.util.BitSet
 
 class Y24D12(input: String) : Day {
@@ -14,16 +13,31 @@ class Y24D12(input: String) : Day {
 
     data class Region(val area: Int, val perimeter: Int, val sides: Int, val surveyed: BitSet) {
         companion object {
+            /**
+             * Finds the region for any given plant in the arrangement. Uses DFS to explore the region,
+             * with each plot looking at adjacent plots and loading plots of the same plant into the queue.
+             * Area and perimeter are calculated as each plot is evaluated. Area is just a count of plots
+             * in the region. Perimeter is the sides that don't have a matching plant adjacent, so
+             * 4 - [# of adjacent matching plants].
+             *
+             * Side are a bit kludgy and are relegated to a separate calculateSides function.
+             */
             fun new(arrangement: Grid<Char>, intPos: Int, plant: Char): Region {
+                // Setting up DFS
                 val start = intPos.toCoord(arrangement.width)
-                val q = ArrayDeque<Coord>()
-                q.addLast(start)
+                val q = mutableListOf(start)
                 val surveyed = mutableSetOf(start)
+
+                // area and perimeter are counted block by block in the DFS loop, so use mutable variables
                 var area = 0
                 var perimeter = 0
+
+                // DFS loop.
                 while (q.isNotEmpty()) {
-                    val current = q.removeFirst()
+                    val current = q.removeLast()
                     area++
+
+                    // get adjacent plots and only look at the ones that have matching plants
                     val neighbors = arrangement
                         .getNeighborsIndexedValue(current)
                         .filter { (_, nPlant) -> nPlant == plant }
@@ -31,6 +45,7 @@ class Y24D12(input: String) : Day {
 
                     perimeter += 4 - neighbors.size
 
+                    // add those plots that haven't already been examined to the queue
                     neighbors
                         .filter { neighbor ->
                             if (neighbor in surveyed) {
@@ -41,12 +56,41 @@ class Y24D12(input: String) : Day {
                             }
                         }.forEach { neighbor -> q.add(neighbor) }
                 }
+
+                val sides = calculateSides(surveyed)
+
+                val bitSet = BitSet(arrangement.size)
+                for (pos in surveyed) {
+                    bitSet[pos.asIndex(arrangement.width)] = true
+                }
+
+                return Region(area, perimeter, sides, bitSet)
+            }
+
+            /**
+             * I can't calculate until the entire region is known because the placement
+             * of non-adjacent plants in the region affects whether a border is a new side. So after the DFS
+             * is finished, I go through each of the points in the region once more, marking each revisit
+             * in a separate set (a "resurvey"). Again, I check to see if there is a direction that doesn't have an
+             * adjacent matching plant. If so, it is part of a side.
+             *
+             * But if we just count all these side parts, that's just the perimeter. We need to count each
+             * side part just once. So for a side part in any given direction, I check adjacent plots to the
+             * right and left. If either one of them has already been "resurveyed", then the side has already
+             * been counted, so I don't add to the count.
+             *
+             * Finally, there can be a situation where the "resurvey" overcounts the number of sides because
+             * a contiguous side can be formed from both ends, neither side knowing that eventually they will be
+             * joined. I resolve this by checking to see if the piece connects two already resurveyed blocks in a
+             * straight line. If it does that, I subtract one.
+             */
+            private fun calculateSides(surveyed: MutableSet<Coord>): Int {
                 val resurveyed = mutableSetOf<Coord>()
                 val sides = surveyed.sumOf { pos ->
                     resurveyed.add(pos)
                     val posSides = Nsew.entries
                         .map { it to pos.move(it) }
-                        .filter { (_, adjacent) -> adjacent !in surveyed  }
+                        .filter { (_, adjacent) -> adjacent !in surveyed }
                         .sumOf { (dir, _) ->
                             val checkRight = checkTurn(dir.right(), dir, pos, surveyed, resurveyed)
                             val checkLeft = checkTurn(dir.left(), dir, pos, surveyed, resurveyed)
@@ -58,13 +102,7 @@ class Y24D12(input: String) : Day {
                         }
                     posSides
                 }
-
-                val bitSet = BitSet(arrangement.size)
-                for (pos in surveyed) {
-                    bitSet[pos.asIndex(arrangement.width)] = true
-                }
-
-                return Region(area, perimeter, sides, bitSet)
+                return sides
             }
 
             private fun checkTurn(turn: Nsew, dir: Nsew, pos: Coord, surveyed: Set<Coord>, resurveyed: Set<Coord>): Boolean {
@@ -87,10 +125,14 @@ class Y24D12(input: String) : Day {
             }
     }
     override fun part1() = solve { area * perimeter }
-
     override fun part2() = solve { area * sides }
 }
-fun main() = Day.runDay(Y24D12::class)//, test[4])
+fun main() = Day.runDay(Y24D12::class)
+
+//    Class creation: 10ms
+//    Part 1: 1424472 (89ms)
+//    Part 2: 870202 (39ms)
+//    Total time: 139ms
 
 //p1 0: 140, 1: 772, 2: 1930
 //p2 0: 80, 1: 436, 2: 1206, 3: 236, 4: 368
