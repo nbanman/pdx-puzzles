e39231cf21e761161e5961b991c51ab0eed16a38
neil <neil.banman@gmail.com>
2024-12-13 00:29:28 -0800

    add counting corners algo

--- kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2024/Y24D12.kt
+++ kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2024/Y24D12.kt
@@ -20,7 +20,9 @@ class Y24D12(input: String) : Day {
              * in the region. Perimeter is the sides that don't have a matching plant adjacent, so
              * 4 - [# of adjacent matching plants].
              *
-             * Side are a bit kludgy and are relegated to a separate calculateSides function.
+             * Side are a bit kludgy and are relegated to a separate corners function. I refactored to use
+             * Todd Ginsberg's counting corners method, which performs about the same but is far more
+             * explicable.
              */
             fun new(arrangement: Grid<Char>, intPos: Int, plant: Char): Region {
                 // Setting up DFS
@@ -31,103 +33,77 @@ class Y24D12(input: String) : Day {
                 // area and perimeter are counted block by block in the DFS loop, so use mutable variables
                 var area = 0
                 var perimeter = 0
+                var corners = 0
 
                 // DFS loop.
                 while (q.isNotEmpty()) {
                     val current = q.removeLast()
+
+                    val neighbors = Nsew.entries.associateWith { dir ->
+                        val nPos = current.move(dir)
+                        val inRegion = plant == arrangement.getOrNull(nPos)
+                        nPos to inRegion
+                    }
+
                     area++
+                    val inRegion = neighbors.values.filter { (_, inRegion) -> inRegion }
+                    perimeter += 4 - inRegion.size
 
-                    // get adjacent plots and only look at the ones that have matching plants
-                    val neighbors = arrangement
-                        .getNeighborsIndexedValue(current)
-                        .filter { (_, nPlant) -> nPlant == plant }
-                        .map { (nPos, _) -> nPos.toCoord(arrangement.width) }
-
-                    perimeter += 4 - neighbors.size
+                    val localCorners = corners(neighbors, arrangement, current, plant)
+                    if (plant == 'C') {
+                        println("$current: $localCorners")
+                    }
+                    corners += localCorners
 
                     // add those plots that haven't already been examined to the queue
-                    neighbors
-                        .filter { neighbor ->
-                            if (neighbor in surveyed) {
-                                false
-                            } else {
-                                surveyed.add(neighbor)
-                                true
-                            }
-                        }.forEach { neighbor -> q.add(neighbor) }
+                    inRegion
+                        .filter { (neighbor, _) -> surveyed.add(neighbor) }
+                        .forEach { (neighbor, _) -> q.add(neighbor) }
                 }
-
-                val sides = calculateSides(surveyed)
-
                 val bitSet = BitSet(arrangement.size)
                 for (pos in surveyed) {
                     bitSet[pos.asIndex(arrangement.width)] = true
                 }
 
-                return Region(area, perimeter, sides, bitSet)
+                return Region(area, perimeter, corners, bitSet)
             }
 
-            /**
-             * I can't calculate until the entire region is known because the placement
-             * of non-adjacent plants in the region affects whether a border is a new side. So after the DFS
-             * is finished, I go through each of the points in the region once more, marking each revisit
-             * in a separate set (a "resurvey"). Again, I check to see if there is a direction that doesn't have an
-             * adjacent matching plant. If so, it is part of a side.
-             *
-             * But if we just count all these side parts, that's just the perimeter. We need to count each
-             * side part just once. So for a side part in any given direction, I check adjacent plots to the
-             * right and left. If either one of them has already been "resurveyed", then the side has already
-             * been counted, so I don't add to the count.
-             *
-             * Finally, there can be a situation where the "resurvey" overcounts the number of sides because
-             * a contiguous side can be formed from both ends, neither side knowing that eventually they will be
-             * joined. I resolve this by checking to see if the piece connects two already resurveyed blocks in a
-             * straight line. If it does that, I subtract one.
-             */
-            private fun calculateSides(surveyed: MutableSet<Coord>): Int {
-                val resurveyed = mutableSetOf<Coord>()
-                val sides = surveyed.sumOf { pos ->
-                    resurveyed.add(pos)
-                    val posSides = Nsew.entries
-                        .map { it to pos.move(it) }
-                        .filter { (_, adjacent) -> adjacent !in surveyed }
-                        .sumOf { (dir, _) ->
-                            val checkRight = checkTurn(dir.right(), dir, pos, surveyed, resurveyed)
-                            val checkLeft = checkTurn(dir.left(), dir, pos, surveyed, resurveyed)
-                            when {
-                                checkRight && checkLeft -> 1
-                                !checkRight && !checkLeft -> -1
-                                else -> 0
-                            }.toInt()
-                        }
-                    posSides
+            private fun corners(
+                neighbors: Map<Nsew, Pair<Coord, Boolean>>,
+                arrangement: Grid<Char>,
+                current: Coord,
+                plant: Char
+            ) = listOf(Nsew.NORTH, Nsew.EAST, Nsew.SOUTH, Nsew.WEST, Nsew.NORTH)
+                .zipWithNext()
+                .count { (a, b) ->
+                    if (!neighbors.getValue(a).second && !neighbors.getValue(b).second) {
+                        true
+                    } else {
+                        neighbors.getValue(a).second
+                                && neighbors.getValue(b).second
+                                && arrangement.getOrNull(current.move(a).move(b)) != plant
+                    }
                 }
-                return sides
-            }
-
-            private fun checkTurn(turn: Nsew, dir: Nsew, pos: Coord, surveyed: Set<Coord>, resurveyed: Set<Coord>): Boolean {
-                val adjacentTurn = pos.move(turn)
-                return adjacentTurn !in resurveyed || adjacentTurn.move(dir) in surveyed
-            }
         }
     }
 
     private fun solve(cost: Region.() -> Int): Int {
         val surveyed = BitSet(arrangement.size)
-        return arrangement
+        val regions = arrangement
             .asSequence()
             .withIndex()
             .filterNot { (pos, _) -> surveyed[pos] }
-            .sumOf { (pos, plant) ->
+            .map { (pos, plant) ->
                 val region = Region.new(arrangement, pos, plant)
                 surveyed.or(region.surveyed)
-                region.cost()
-            }
+                region
+            }.toList()
+        return regions.sumOf { it.cost() }
     }
     override fun part1() = solve { area * perimeter }
     override fun part2() = solve { area * sides }
 }
-fun main() = Day.runDay(Y24D12::class)
+fun main() = Day.runDay(Y24D12::class, test[0])
 
 //    Class creation: 10ms
 //    Part 1: 1424472 (89ms)
