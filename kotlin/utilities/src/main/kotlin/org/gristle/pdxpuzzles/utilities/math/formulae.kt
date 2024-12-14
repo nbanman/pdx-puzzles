b57608f81aa04aa26aac763a67f27754e8f6f147
neil <neil.banman@gmail.com>
2024-12-14 11:59:02 -0800

    removed unused and confusing crt function

--- kotlin/utilities/src/main/kotlin/org/gristle/pdxpuzzles/utilities/math/formulae.kt
+++ kotlin/utilities/src/main/kotlin/org/gristle/pdxpuzzles/utilities/math/formulae.kt
@@ -7,15 +7,6 @@ import kotlin.math.abs
 import kotlin.math.pow
 import kotlin.math.sqrt
 
-fun List<Pair<Long, Long>>.crt(): Long {
-    val n = fold(1L) { acc, (mod, _) -> acc * mod }
-    val bigPhase = sumOf { (mod, remainder) ->
-        val ni = (n / mod)
-        remainder * ni * (ni.toBigInteger().modInverse(remainder.toBigInteger())).toLong()
-    }
-    return bigPhase % n
-}
-
 fun Int.isEven() = this and 1 == 0
 fun Int.isOdd() = this and 1 == 1
 
