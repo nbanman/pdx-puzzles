2641745dec11ae8dba68357edb026444c6b7f9e5
neil <neil.banman@gmail.com>
2024-12-17 17:03:15 -0800

    remove day 1

--- kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2024/Y24D17.kt
+++ kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2024/Y24D17.kt
@@ -5,25 +5,11 @@ import org.gristle.pdxpuzzles.advent.utilities.iteration.collate
 import org.gristle.pdxpuzzles.utilities.parsing.getIntList
 import kotlin.math.abs
 
-class Y24D01(private val input: String) : Day {
+class Y24D17(input: String) : Day {
 
-    override fun part1() = input
-        .getIntList()
-        .collate(2)
-        .map { it.sorted() }
-        .let { (a, b) -> a zip b }
-        .sumOf { (a, b) -> abs(a - b) }
+    override fun part1() = 3
 
-    override fun part2(): Int {
-        val (a, b) = input.getIntList().collate(2)
-        val freq = b.groupingBy { it }.eachCount()
-        return a.sumOf { it * (freq[it] ?: 0) }
-    }
+    override fun part2() = 3
 }
 
-fun main() = Day.runDay(Y24D01::class)
-
-//    Class creation: 1ms
-//    Part 1: 1222801 (11ms)
-//    Part 2: 22545250 (6ms)
-//    Total time: 20ms
+fun main() = Day.runDay(Y24D17::class)
