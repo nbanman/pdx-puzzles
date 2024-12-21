8bf0afda9374b5e7807b6e9711183cf576c76113
neil <neil.banman@gmail.com>
2024-12-20 19:20:33 -0800

    initial working

--- kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2024/Y24D20.kt
+++ kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2024/Y24D20.kt
@@ -5,6 +5,7 @@ import org.gristle.pdxpuzzles.utilities.iteration.minMax
 import org.gristle.pdxpuzzles.utilities.objects.Coord
 import org.gristle.pdxpuzzles.utilities.objects.toCoord
 import java.util.ArrayDeque
+import kotlin.math.min
 
 class Y24D20(private val racetrack: String) : Day {
     private val width = racetrack.indexOf('\n') + 1
@@ -18,7 +19,7 @@ class Y24D20(private val racetrack: String) : Day {
     private val honestDistance = fromStart[end]
     private val threshold = honestDistance - 100
 
-    private inline fun<T> bfs(start: Int, end: Int?, visited: T, edges: (Int, T, Int) -> List<Pair<Int, Int>>): T {
+    private inline fun <T> bfs(start: Int, end: Int?, visited: T, edges: (Int, T, Int) -> List<Pair<Int, Int>>): T {
         val q = ArrayDeque<Pair<Int, Int>>()
         q.addLast(0 to start)
 
@@ -43,6 +44,7 @@ class Y24D20(private val racetrack: String) : Day {
                 visited[neighbor] = steps + 1
                 null
             }
+
             else -> {
                 val neighborSteps = steps + 1
                 visited[neighbor] = neighborSteps
@@ -55,59 +57,59 @@ class Y24D20(private val racetrack: String) : Day {
         pos: Int,
         endCoord: Coord,
         steps: Int,
-    ): Pair<Int, List<Int>> {
-        if (racetrack[pos] == '#') return 0 to emptyList()
+    ): Int {
+        if (racetrack[pos] == '#') return 0
 
         val posCoord = pos.toCoord(width)
         val difference = endCoord - posCoord
         val minSteps = steps + difference.manhattanDistance()
 
-        if (minSteps > threshold) return 0 to emptyList()
+        if (minSteps > threshold) return 0
 
         var count = 0
         val savings = mutableListOf<Int>()
         val allowance = (threshold - minSteps) / 2
         val (north, south) = minMax(posCoord.y, endCoord.y)
-        val yRange = (north - allowance).coerceAtLeast(1)..
-                (south + allowance).coerceAtMost(height - 2)
+        val yRange = (north - min(20, allowance)).coerceAtLeast(1)..
+                (south + min(20, allowance)).coerceAtMost(height - 2)
         for (y in yRange) {
+            val xMin = min(20, allowance)
             val xAllowance = when {
-                y < north -> allowance - (north - y)
-                y > south -> allowance - (y - south)
-                else -> allowance
+                y < north -> xMin - (north - y)
+                y > south -> xMin - (y - south)
+                else -> xMin
             }
             val (west, east) = minMax(posCoord.x, endCoord.x)
-            val xRange = (west - allowance).coerceAtLeast(1)..
-                    (east + allowance).coerceAtMost(width - 3)
+            val xRange = (west - xAllowance).coerceAtLeast(1)..
+                    (east + xAllowance).coerceAtMost(width - 3)
             for (x in xRange) {
                 val rePos = y * width + x
                 if (racetrack[rePos] == '#') continue
                 val cheatSteps = steps + posCoord.manhattanDistance(Coord(x, y)) + fromEnd[rePos]
-                if (cheatSteps <= threshold) {
+                if (cheatSteps <= threshold && posCoord.manhattanDistance(Coord(x, y)) <= 20) {
                     count++
                     savings.add(honestDistance - cheatSteps)
                 }
             }
         }
-        return count to savings
+        return count
     }
 
     override fun part1(): Int = fromStart.withIndex().count { (pos, steps) ->
         racetrack[pos] == '#' && steps + fromEnd[pos] <= threshold
     }
 
-    override fun part2() = let {
-        val endCoord = end.toCoord(width)
-        val test = fromStart.withIndex().map { (pos, steps) ->
-            (pos to steps) to countValid(pos, endCoord, steps)
-        }
-        test.sumOf { it.second.first }
+    override fun part2() = fromStart.withIndex().sumOf { (pos, steps) ->
+        countValid(pos, end.toCoord(width), steps)
     }
 }
 
-fun main() = Day.runDay(Y24D20::class)
-// 25536 too low
-// 42903515 too high
+fun main() = Day.benchmarkDay(Y24D20::class)
+
+//    Class creation: 9ms
+//    Part 1: 1406 (4ms)
+//    Part 2: 1006101 (243ms)
+//    Total time: 257ms
 
 @Suppress("unused")
 private val test = listOf("""###############
