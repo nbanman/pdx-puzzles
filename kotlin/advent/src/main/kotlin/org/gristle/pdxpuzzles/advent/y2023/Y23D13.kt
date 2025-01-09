f4adbc3be485c7b026f4e472bbe882c91db7cf0e
neil <neil.banman@gmail.com>
2025-01-09 09:52:16 -0800

    refactor

--- kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2023/Y23D13.kt
+++ kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2023/Y23D13.kt
@@ -1,137 +1,52 @@
 package org.gristle.pdxpuzzles.advent.y2023
 
 import org.gristle.pdxpuzzles.advent.utilities.Day
-import org.gristle.pdxpuzzles.utilities.objects.Grid
 import org.gristle.pdxpuzzles.utilities.objects.toGrid
 import org.gristle.pdxpuzzles.utilities.parsing.blankSplit
 
 class Y23D13(input: String) : Day {
+    data class Pattern(val rows: List<List<Boolean>>, val cols: List<List<Boolean>>) {
+        fun seamSummary(smudged: Boolean): Int =
+            findSeam(true, smudged)
+                ?: findSeam(false, smudged)?.times(100)
+                ?: throw IllegalStateException("No seam found!")
 
-    // The value for part 1 is used in part 2, so make it a class value. We want representative times for part 1,
-    // so make the value lazy. This parses by splitting on blank lines, turning each split into a Grid, then
-    // running the findSeam function using Grid components. The Grid and the answer are returned in a Pair.
-    private val patterns: List<Pair<Grid<Boolean>, Int>> by lazy {
-        input.blankSplit()
-            .map { s ->
-                val pattern = s.toGrid { it == '#' }
-                with(pattern) {
-                    val value = findSeam(width, columns(), 1)
-                        ?: findSeam(height, rows(), 100)
-                        ?: throw IllegalStateException("No seam found in columns or rows.")
-                    pattern to value
+        private fun findSeam(isHz: Boolean, smudged: Boolean): Int? {
+            val smudge = if (smudged) 1 else 0
+            val lines = if (isHz) cols else rows
+
+            for (i in 0 until lines.lastIndex) {
+                var diff = 0
+                for (j in 0..i) {
+                    if (i + j + 1 == lines.size) break
+                    diff += (lines[i - j] zip lines[i + j + 1]).count { (aa, bb) -> aa != bb }
+                    if (diff > smudge) break
                 }
+                if (smudge == diff) return i + 1
             }
-    } 
-    
-    // Finds the initial seam. It uses a stack that starts at the left/top and keeps adding lines until
-    // a duplicate is found. That is a potential seam. It verifies the seam by popping lines off the stack and
-    // comparing them to the next lines. If the stack value and the next line value don't add up, it's not a valid
-    // seam, and the values are reset and the outer loop is resumed. If it reaches the end of the stack or the 
-    // end of the lines, the seam is valid and that value is returned.  
-    private fun findSeam(length: Int, lines: List<List<Boolean>>, factor: Int): Int? {
-        val stack: MutableList<List<Boolean>> = ArrayList(length - 1)
-        stack.add(lines.first())
-        var potentialSeam = -1
-        var location = 1
-        loop@while (location in lines.indices) {
-            val next = lines[location]
-            if (next != stack.last()) {
-                stack.add(next)
-                location++
-            } else { // potential seam found
-                potentialSeam = location
-                inner@while (true) {
-                    stack.removeLast()
-                    if (stack.isEmpty()) break@loop
-                    val innerNext = lines.getOrNull(++location) ?: break@loop
-                    if (stack.last() != innerNext) { // potential seam not real
-                        location = potentialSeam - (location - potentialSeam)
-                        while (location != potentialSeam + 1) {
-                            stack.add(lines[location])
-                            location++
-                        }
-                        potentialSeam = -1
-                        break@inner
-                    }
-                }
-                
+            return null
+        }
+
+        companion object {
+            fun new(s: String): Pattern {
+                val mirrors = s.toGrid { it == '#' }
+                return Pattern(mirrors.rows(), mirrors.columns())
             }
         }
-        return if (potentialSeam == -1) null else potentialSeam * factor
     }
 
-    // Finds the seam with a smudge. The logic is the same as findSeam, except for two things. First, it will
-    // ignore the seam if the index is passed in by the 'ignore' parameter. This prevents finding the old seam.
-    // Second, when making comparisons, it will allow a difference of 1, but only once for any potential seam.
-    // So if 'smudge' is used to identify the potential seam, then the popping/comparison phase must have 
-    // identical comparisons. If not, then the popping/comparison phase can have one comparison with a difference
-    // of one. This is tracked with a 'smudged' Boolean variable.
-    private fun findSmudgeSeam(length: Int, lines: List<List<Boolean>>, factor: Int, ignore: Int?): Int? {
-        val stack: MutableList<List<Boolean>> = ArrayList(length - 1)
-        stack.add(lines.first())
-        var potentialSeam = -1
-        var location = 1
-        var smudged = false
-        loop@while (location in lines.indices) {
-            val next = lines[location]
-            val difference = next.difference(stack.last())
-            if (difference > 1 || (difference == 0 && ignore == location)) {
-                stack.add(next)
-                location++
-            } else { // potential seam found
-                if (difference == 1) smudged = true
-                potentialSeam = location
-                inner@while (true) {
-                    stack.removeLast()
-                    if (stack.isEmpty()) break@loop
-                    val innerNext = lines.getOrNull(++location) ?: break@loop
-                    val innerDifference = innerNext.difference(stack.last())
-                    if (innerDifference > 1 || (innerDifference == 1 && smudged)) { // potential seam not real
-                        smudged = false
-                        location = potentialSeam - (location - potentialSeam)
-                        while (location != potentialSeam + 1) {
-                            stack.add(lines[location])
-                            location++
-                        }
-                        potentialSeam = -1
-                        break@inner
-                    }
-                    if (innerDifference == 1) smudged = true
-                }
+    private val patterns: List<Pattern> = input.blankSplit().map(Pattern::new)
 
-            }
-        }
-        return if (potentialSeam == -1) null else potentialSeam * factor
-    }
-    
-    // Utility function to return the number of different elements between two lists.
-    private fun List<Boolean>.difference(other: List<Boolean>): Int {
-        return (this zip other).count { (a, b) -> a != b }
-    }
-
-    override fun part1() = patterns.sumOf { (_, value) -> value }
-
-    override fun part2() = patterns
-        .sumOf { (pattern, value) ->
-            // The indices of previous seams to ignore. 
-            val ignoreHorizontal = if (value < 100) value else null
-            val ignoreVertical = if (value >= 100) value / 100 else null
-            
-            with (pattern) {
-                val answer = findSmudgeSeam(width, columns(), 1, ignoreHorizontal)
-                    ?: findSmudgeSeam(height, rows(), 100, ignoreVertical)
-                    ?: throw IllegalStateException("No seam found in columns or rows.")
-                answer
-            }
-        }
+    override fun part1() = patterns.sumOf { it.seamSummary(false) }
+    override fun part2() = patterns.sumOf { it.seamSummary(true) }
 }
 
 fun main() = Day.runDay(Y23D13::class)
 
-//    Class creation: 2ms
-//    Part 1: 27505 (17ms)
-//    Part 2: 22906 (10ms)
-//    Total time: 30ms
+//    Class creation: 8ms
+//    Part 1: 27505 (4ms)
+//    Part 2: 22906 (2ms)
+//    Total time: 15ms
 
 @Suppress("unused")
 private val sampleInput = listOf(
--- rust/advent/src/bin/2023/y23d13.rs
+++ rust/advent/src/bin/2023/y23d13.rs
@@ -1,5 +1,3 @@
-use std::ops::Add;
-
 use advent::utilities::get_input::get_input;
 use utilities::structs::stopwatch::{ReportDuration, Stopwatch};
 
@@ -17,99 +15,70 @@ fn main() {
     println!("Total: {}", stopwatch.stop().report());
 }
 
+// Stores data in both rows and columns for easy operation.
 #[derive(Debug)]
-struct Pattern {
-    rows: Vec<Vec<bool>>,
-    cols: Vec<Vec<bool>>,
-}
+struct Pattern { rows: Vec<Vec<bool>>, cols: Vec<Vec<bool>>, }
 
 impl Pattern {
+    fn new(s: &str) -> Self {
+        let width = s.find('\n').unwrap();
+        
+        // intermediate representation
+        let mirrors: Vec<bool> = s.as_bytes().iter()
+            .filter(|&&c| c != b'\n')
+            .map(|&c| c == b'#')
+            .collect();
+        let height = mirrors.len() / width;
+        let rows = (0..height)
+            .map(|n| {
+                let offset = n * width;
+                mirrors[offset..offset + width].iter()
+                    .copied()
+                    .collect()
+            })
+            .collect();
+        let cols = (0..width)
+            .map(move |n| {
+                (0..height).map(|row| mirrors[row * width + n]).collect() 
+            })
+            .collect();
+        Pattern { rows, cols } 
+    }
+    
+    // calls find_seam for columns first. If a seam is found, return the value. 
+    // Otherwise, calls find_seam for rows. If a seam is found, return the value * 100.
+    // Otherwise, panic!
     fn seam_summary(&self, smudged: bool) -> usize {
-        if let Some(hz_seam) = self.find_seam(true, smudged) {
-            hz_seam
-        } else {
-            self.find_seam(false, smudged).unwrap() * 100
-        }
+        self.find_seam(true, smudged)
+            .unwrap_or_else(|| self.find_seam(false, smudged).unwrap() * 100)
     }
 
+    // Goes through each "line" (either a row or column), and compares it with the next one.
+    // If there is a mirrored line, checks if the mirroring continues on either side. If it 
+    // continues until there are no more lines to be compared, a seam is found.
+    // 
+    // In part 2, there is one line that has a difference of 1 (the smudge). So track 
+    // differences with "diff" and proceed accordingly.
     fn find_seam(&self, is_hz: bool, smudged: bool) -> Option<usize> {
+        let smudge = if smudged { 1 } else { 0 };
         let lines = if is_hz { &self.cols } else { &self.rows };
         for i in 0..lines.len() - 1 {
-            let initial_diff = Pattern::diff(&lines[i], &lines[i + 1]);
-            if initial_diff == Diff::Zero || (initial_diff == Diff::One && smudged) {
-                let mut diff = initial_diff;
-                for j in 1..=i {
-                    if j + i + 1 == lines.len() { break; }
-                    diff = diff + Pattern::diff(&lines[i - j], &lines[i + j + 1]);
-                    if diff == Diff::More || (diff == Diff::One && !smudged) { break; }
-                }
-                if smudged && diff == Diff::One { return Some(i + 1); }
-                if !smudged && diff == Diff::Zero { return Some(i + 1); }
+            let mut diff = 0;
+            for j in 0..=i {
+                if i + j + 1 == lines.len() { break; }
+                diff += lines[i - j].iter().zip(lines[i + j + 1].iter())
+                    .filter(|(&aa, &bb)| aa != bb)
+                    .count();
+                if diff > smudge { break; }
             }
+            if smudge == diff { return Some(i + 1)}; 
         }
         None
     }
-
-    fn diff(a: &[bool], b: &[bool]) -> Diff {
-        match a.iter().zip(b.iter()).filter(|(&aa, &bb)| aa != bb).count() {
-            0 => Diff::Zero,
-            1 => Diff::One,
-            _ => Diff::More,
-        }
-    }
 }
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-enum Diff {
-    Zero,
-    One,
-    More,
-}
-
-impl Add for Diff {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        match self {
-            Diff::Zero => {
-                match rhs {
-                    Diff::Zero => Diff::Zero,
-                    Diff::One => Diff::One,
-                    Diff::More => Diff::More,
-                }
-            },
-            Diff::One => if rhs == Diff::Zero { Diff:: One } else { Diff::More },
-            Diff::More => Diff::More,
-        }
-    }
-}
-
 
 fn parse_input(input: &str) -> Input {
-    input.split("\n\n")
-        .map(|stanza| {
-            let width = stanza.find('\n').unwrap();
-            let mirrors: Vec<bool> = stanza.as_bytes().iter()
-                .filter(|&&c| c != b'\n')
-                .map(|&c| c == b'#')
-                .collect();
-            let height = mirrors.len() / width;
-            let rows = (0..height)
-                .map(|n| {
-                    let offset = n * width;
-                    mirrors[offset..offset + width].iter()
-                        .copied()
-                        .collect()
-                })
-                .collect();
-            let cols = (0..width)
-                .map(move |n| {
-                    (0..height).map(|row| mirrors[row * width + n]).collect() 
-                })
-                .collect();
-            Pattern { rows, cols } 
-        })
-        .collect()
+    input.split("\n\n").map(Pattern::new).collect()
 }
 
 fn part1(patterns: &Input) -> Output {
