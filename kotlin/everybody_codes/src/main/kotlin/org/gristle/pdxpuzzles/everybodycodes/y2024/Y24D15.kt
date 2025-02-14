c72acaad2be6b981b9aae165675f1f76ef6ae463
nbanman <neil.banman@gmail.com>
2025-02-14 13:39:51 -0800

    fixed ec directory lookup

---
+++ kotlin/everybody_codes/src/main/kotlin/org/gristle/pdxpuzzles/everybodycodes/y2024/Y24D15.kt
@@ -0,0 +1,11 @@
+package org.gristle.pdxpuzzles.everybodycodes.y2024
+
+import org.gristle.pdxpuzzles.everybodycodes.utilities.Day
+
+object Y24DXX : Day {
+    override fun part1(input: String): Int = 1
+    override fun part2(input: String): Int = 2
+    override fun part3(input: String): Int = 3
+}
+
+fun main() = Day.runDay(Y24DXX::class)
