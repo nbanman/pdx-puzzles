041853d75edbda861909e5e395bdf91101581e78
nbanman <neil.banman@gmail.com>
2025-02-24 12:24:33 -0800

    rustfmt

--- rust/advent/src/bin/2024/y24d01.rs
+++ rust/advent/src/bin/2024/y24d01.rs
@@ -2,7 +2,10 @@ use std::collections::HashMap;
 
 use advent::utilities::get_input::get_input;
 use itertools::{Either, Itertools};
-use utilities::{parsing::get_numbers::ContainsNumbers, structs::stopwatch::{ReportDuration, Stopwatch}};
+use utilities::{
+    parsing::get_numbers::ContainsNumbers,
+    structs::stopwatch::{ReportDuration, Stopwatch},
+};
 
 type Input = (Vec<usize>, Vec<usize>);
 type Output = usize;
@@ -18,33 +21,32 @@ fn main() {
 }
 
 fn parse_input(input: &str) -> Input {
-    input.get_numbers::<usize>().enumerate().partition_map(|(idx, n)| {
-        if idx & 1 == 0 {
-            Either::Left(n)
-        } else {
-            Either::Right(n)
-        }
-    })
+    input
+        .get_numbers::<usize>()
+        .enumerate()
+        .partition_map(|(idx, n)| {
+            if idx & 1 == 0 {
+                Either::Left(n)
+            } else {
+                Either::Right(n)
+            }
+        })
 }
 
 fn part1(input: Input) -> Output {
     let (mut a, mut b) = input;
     a.sort_unstable();
     b.sort_unstable();
-    a.into_iter().zip(b)
-        .map(|(a, b)| a.abs_diff(b))
-        .sum()
+    a.into_iter().zip(b).map(|(a, b)| a.abs_diff(b)).sum()
 }
 
 fn part2(input: Input) -> Output {
     let (a, b) = input;
     let mut freq = HashMap::new();
     for n in b {
-        freq.entry(n)
-            .and_modify(|count| { *count += 1 })
-            .or_insert(1);
+        freq.entry(n).and_modify(|count| *count += 1).or_insert(1);
     }
-    a.into_iter().map(|n| n * freq.get(&n).unwrap_or(&0)).sum()   
+    a.into_iter().map(|n| n * freq.get(&n).unwrap_or(&0)).sum()
 }
 
 #[test]
@@ -54,7 +56,6 @@ fn default() {
     assert_eq!(22545250, part2(input));
 }
 
-
 #[test]
 fn examples() {
     let inputs = [r"3   4
@@ -63,7 +64,7 @@ fn examples() {
 1   3
 3   9
 3   3
-", ];
+"];
     let input = parse_input(inputs[0]);
     assert_eq!(11, part1(input.clone()));
     assert_eq!(31, part2(input));
