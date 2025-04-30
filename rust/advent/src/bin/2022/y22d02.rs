f288a6a612400806835f9f203d31da2f3b40d55e
nbanman <neil.banman@gmail.com>
2025-04-29 17:37:48 -0700

    used single solve fn

--- rust/advent/src/bin/2022/y22d02.rs
+++ rust/advent/src/bin/2022/y22d02.rs
@@ -29,6 +29,18 @@ fn parse_input(input: &str) -> Input {
         .collect()
 }
 
+fn solve<F>(rounds: &Input, f: F) -> Int 
+where F: Fn(Int, Int) -> (Int, Int),
+{
+    rounds
+        .iter()
+        .map(|&(opponent_throw, my)| {
+            let (my_outcome, my_throw) = f(opponent_throw, my);
+            outcome_score(my_outcome) + throw_score(my_throw)
+        })
+        .sum()
+}
+
 fn throw_score(my_throw: Int) -> Int {
     my_throw + 1
 }
@@ -37,32 +49,18 @@ fn outcome_score(my_outcome: Int) -> Int {
     my_outcome * 3
 }
 
-fn my_outcome(my_throw: Int, opponent_throw: Int) -> Int {
-    (my_throw - opponent_throw + 1).rem_euclid(3)
-}
-
-fn my_throw(my_outcome: Int, opponent_throw: Int) -> Int {
-    (my_outcome + opponent_throw - 1).rem_euclid(3)
-}
-
 fn part1(rounds: &Input) -> Int {
-    rounds
-        .iter()
-        .map(|&(opponent_throw, my_throw)| {
-            let my_outcome = my_outcome(my_throw, opponent_throw);
-            outcome_score(my_outcome) + throw_score(my_throw)
-        })
-        .sum()
+    solve(rounds, |opponent_throw, my_throw| {
+        let my_outcome = (my_throw - opponent_throw + 1).rem_euclid(3);
+        (my_outcome, my_throw)
+    })
 }
 
 fn part2(rounds: &Input) -> Int {
-    rounds
-        .iter()
-        .map(|&(opponent_throw, my_outcome)| {
-            let my_throw = my_throw(my_outcome, opponent_throw);
-            outcome_score(my_outcome) + throw_score(my_throw)
-        })
-        .sum()
+    solve(rounds, |opponent_throw, my_outcome| {
+        let my_throw = (my_outcome + opponent_throw - 1).rem_euclid(3);
+        (my_outcome, my_throw)
+    })
 }
 
 #[test]
