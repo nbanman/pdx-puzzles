ed5dc0a11386b1f92d3f80d074381ea5c4d62de4
nbanman <neil.banman@gmail.com>
2025-09-08 17:19:39 -0700

    added display trait

---
+++ rust/utilities/src/structs/grid/grid_display.rs
@@ -0,0 +1,17 @@
+use std::fmt::{Display, Formatter};
+use crate::structs::grid::Grid;
+
+impl Display for Grid<char, 2> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut display = String::new();
+        let mut iter = self.iter();
+        for _y in 0..self.height() {
+            for _x in 0..self.width() {
+                display.push(*iter.next().expect("Iterator should never be empty"));
+            }
+            display.push('\n');
+        }
+        display.pop();
+        write!(f, "{}", display)
+    }
+}
