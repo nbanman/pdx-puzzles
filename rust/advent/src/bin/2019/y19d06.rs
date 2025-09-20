d006adf8041b12a9009ba300325b3f27e6feaee5
nbanman <neil.banman@gmail.com>
2025-09-20 11:19:46 -0700

    initial

--- rust/advent/src/bin/2019/y19d06.rs
+++ rust/advent/src/bin/2019/y19d06.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 
 use advent::utilities::get_input::get_input;
+use itertools::Itertools;
 use utilities::structs::stopwatch::{ReportDuration, Stopwatch};
 
-type Input = HashMap<String, CelestialBody>;
+type Input = HashMap<String, Vec<String>>;
 type Output = usize;
 
 fn main() {
@@ -17,33 +18,48 @@ fn main() {
     println!("Total: {}", stopwatch.stop().report());
 }
 
-struct CelestialBody {
-    name: String,
-    parent: Option<String>,
-    orbits: Option<usize>,
-}
 fn parse_input(input: &str) -> Input {
-    let com_name = "COM".to_string();
-    let com = (com_name.clone(), CelestialBody { name: com_name, parent: None, orbits: Some(0)})
-    input.lines()
-        .map(|line| {
-            let (name, parent) = line.split_once(')').unwrap()
-            let name = name.to_string();
-            let parent = Some(parent.to_string());
-            (name.clone(), CelestialBody { name, parent, orbits: None })
-        })
-        .chain(std::iter::once(com))
-        .collect()
+    let mut children: HashMap<String, Vec<String>> = HashMap::new();
+    let mut paths: HashMap<String, Vec<String>> = HashMap::new();
+    for line in input.lines() {
+        let (parent, name) = line.split(')')
+            .map(|s| s.to_string())
+            .collect_tuple()
+            .unwrap();
+        children.entry(parent.clone())
+            .or_insert(Vec::new())
+            .push(name.clone());
+    }
+    develop_paths("COM", Vec::new(), &mut paths, &children);
+    paths
 }
 
-fn part1(input: &Input) -> Output {
 
-    todo!()
+fn develop_paths(
+    planet: &str,
+    prev: Vec<String>,
+    paths: &mut HashMap<String, Vec<String>>,
+    register: &HashMap<String, Vec<String>>,
+) {
+    let mut next = prev.clone();
+    let planet_string = planet.to_string();
+    next.push(planet_string.clone());
+    paths.insert(planet_string, prev);
+    let Some(children) = register.get(planet) else { return; };
+    for child in children {
+        develop_paths(child, next.clone(), paths, register);
+    }
 }
 
-fn part2(input: &Input) -> Output {
+fn part1(paths: &Input) -> Output {
+    paths.values().map(|x| x.len()).sum()
+}
 
-    todo!()
+fn part2(paths: &Input) -> Output {
+    let me = paths.get("YOU").unwrap();
+    let santa = paths.get("SAN").unwrap();
+    let shared_size = (0..me.len()).find(|&i| me[i] != santa[i]).unwrap();
+    me.len() + santa.len() - shared_size * 2
 }
 
 #[test]
@@ -51,6 +67,10 @@ fn default() {
     let input = get_input(19, 6).unwrap();
     let input = parse_input(&input);
     assert_eq!(315757, part1(&input));
-    // assert_eq!(481, part2(&input));
+    assert_eq!(481, part2(&input));
 }
 
+// Input parsed (21ms)
+// 1. 315757 (10μs)
+// 2. 481 (2μs)
+// Total: 21ms
