22d930c3e3b004689595d128dfd8314d56fdd020
nbanman <neil.banman@gmail.com>
2025-09-24 09:01:19 -0700

    initial

--- rust/advent/src/bin/2019/y19d15.rs
+++ rust/advent/src/bin/2019/y19d15.rs
@@ -1,9 +1,11 @@
+use std::mem;
 use advent::utilities::{get_input::get_input, intcode::{IntCode, State}};
-use utilities::{enums::cardinals::Cardinal, structs::{coord::Coord2U, grid::Grid2, stopwatch::{ReportDuration, Stopwatch}}};
+use rustc_hash::FxHashSet;
+use utilities::{enums::cardinals::Cardinal, structs::{coord::Coord2, stopwatch::{ReportDuration, Stopwatch}}};
 
-type Input = Grid2<Sector>;
+type Input = (FxHashSet<Pos>, Pos);
 type Output = usize;
-type Pos = Coord2U;
+type Pos = Coord2;
 
 fn main() {
     let mut stopwatch = Stopwatch::new();
@@ -16,69 +18,86 @@ fn main() {
     println!("Total: {}", stopwatch.stop().report());
 }
 
-#[derive(Debug, Copy, Clone)]
-enum Sector { Unexplored, Wall, Path, O2}
-
 fn parse_input(input: &str) -> Input {
     let mut compy = IntCode::from(input);
-    let mut ship = Grid2::new2d_with_fn(1_000, 1_000, |_| Sector::Unexplored);
-    let start_pos = Pos::new2d(500, 500);
-    ship[start_pos] = Sector::Path;
+    let mut ship = FxHashSet::default();
+    let start_pos = Pos::origin();
+    let mut o2 = start_pos;
+    ship.insert(start_pos);
     let mut pos = start_pos;
     let mut dir = Cardinal::North;
-    
+    let mut attempt = 0;
     loop {
+        dir = if attempt == 0 {
+            dir.right()
+        } else {
+            dir.left()
+        };
+        attempt += 1;
         compy.input(dir_to_input(dir));
         let State::Output(output) = compy.run() else {
             panic!("Computer failed to return output")
         };
-        let attempted_pos = pos.move_direction(dir, 1).unwrap();
-        match output_to_sector(output) {
-            Sector::Wall => {
-                ship[attempted_pos] = Sector::Wall;
-                dir = dir.right();
-            },
-            Sector::Path => {
-                pos = attempted_pos;
-                ship[pos] = Sector::Path;
-            },
-            Sector::O2 => {
-                pos = attempted_pos;
-                ship[pos] = Sector::O2;
-            }
-            Sector::Unexplored => { panic!("Computer returned \"unexplored\""); },
+        if output == 0 { continue; }
+
+        attempt = 0;
+        pos = pos.move_direction(dir, 1).unwrap();
+        ship.insert(pos);
+        if output == 2 {
+            o2 = pos;
         }
-        if pos == start_pos { break; }
+        if pos == start_pos {
+            break;
+        }
     }
-    ship
+    (ship, o2)
 }
 
 fn dir_to_input(dir: Cardinal) -> i64 {
     match dir {
         Cardinal::North => 1,
-        Cardinal::East => 2,
-        Cardinal::South => 3,
-        Cardinal::West => 4,
+        Cardinal::East => 4,
+        Cardinal::South => 2,
+        Cardinal::West => 3,
     }
 }
 
-fn output_to_sector(output: i64) -> Sector {
-    match output {
-        0 => Sector::Wall,
-        1 => Sector::Path,
-        2 => Sector::O2,
-        d => { panic!("{d} is not a valid output"); },
+fn solve<F>(mut ship: FxHashSet<Pos>, start: Pos, end_condition: F) -> Output
+where
+    F: Fn(Pos) -> bool,
+{
+    ship.remove(&start);
+    let mut steps = 0;
+    let mut todo = Vec::new();
+    todo.push(start);
+    let mut next = Vec::new();
+    while !todo.is_empty() {
+        for pos in todo.drain( .. ) {
+            for neighbor in Pos::adjacent(&pos, false) {
+                if ship.remove(&neighbor) {
+                    if end_condition(neighbor) {
+                        return steps + 1;
+                    }
+                    next.push(neighbor);
+                }
+            }
+        }
+        mem::swap(&mut todo, &mut next);
+        steps += 1;
     }
+    steps - 1
 }
 
 fn part1(input: &Input) -> Output {
-
-    todo!()
+    let (ship, o2) = input;
+    let ship = ship.clone();
+    solve(ship, Pos::origin(), |pos| pos == *o2)
 }
 
 fn part2(input: &Input) -> Output {
-
-    todo!()
+    let (ship, o2) = input;
+    let ship = ship.clone();
+    solve(ship, *o2, |_| false)
 }
 
 #[test]
@@ -86,6 +105,10 @@ fn default() {
     let input = get_input(19, 15).unwrap();
     let input = parse_input(&input);
     assert_eq!(250, part1(&input));
-    // assert_eq!(332, part2(&input));
+    assert_eq!(332, part2(&input));
 }
 
+// Input parsed (663μs)
+// 1. 250 (28μs)
+// 2. 332 (57μs)
+// Total: 751μs
