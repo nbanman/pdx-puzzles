56396ad2dddbda94d648f6a7141b1213915d1949
nbanman <neil.banman@gmail.com>
2025-10-03 08:09:54 -0700

    changed from string to bools and Ints

--- kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2018/Y18D12.kt
+++ kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2018/Y18D12.kt
@@ -6,38 +6,33 @@ class Y18D12(input: String) : Day {
     private val lines = input.lines()
 
     // parse initial row of plants
-    private val initialRow = lines[0].drop(15)
+    private val initialRow = lines[0].drop(15).map { it == '#' }.toBooleanArray()
 
-    // parse commands into translation map
-    private val commands: Map<String, Char> = buildMap {
-        lines
-            .drop(2)
-            .filter { it.last() == '#' }
-            .forEach { put(it.take(5), it.last()) }
-    }
+    private val commands = lines
+        .drop(2)
+        .filter { it.last() == '#' }
+        .map { line ->
+            line.take(5).fold(0) { acc, ch -> (acc shl 1) + if (ch == '#') 1 else 0 }
+        }.toSet()
 
     // sequence that provides successive generations of plant rows
     private val generator = generateSequence(initialRow) { it ->
-        val plants = "..$it.." // pad the row to account for growth
-        plants // build the next generation
-            .mapIndexed { index, _ ->
-                val pattern = buildString { // make a pattern using the spot and the spots around it
-                    append(if (index - 2 < 0) '.' else plants[index - 2])
-                    append(if (index - 1 < 0) '.' else plants[index - 1])
-                    append(plants[index])
-                    append(if (index + 1 > plants.lastIndex) '.' else plants[index + 1])
-                    append(if (index + 2 > plants.lastIndex) '.' else plants[index + 2])
-                }
-                // use the translation map with the pattern to determine if there is a plant at that spot
-                if (commands[pattern] != null) '#' else '.'
-            }.joinToString("")
+        BooleanArray(it.size + 4) { i ->
+            val index = i - 2
+            val rng = (index - 2).coerceAtLeast(0)..(index + 2).coerceAtMost(it.lastIndex)
+            var pattern = rng.fold(0) { acc, i ->
+                (acc shl 1) + if (it[i]) 1 else 0
+            }
+            pattern = pattern shl (index + 2 - it.lastIndex).coerceAtLeast(0)
+            pattern in commands
+        }
     }
 
     /**
      * take the row and obtain the sum of pot numbers. "generations" is used to account for growth of the row
      */
-    private fun String.sumOfPotNumbers(generations: Int) =
-        mapIndexed { index, c -> if (c == '#') (index - (generations * 2)) else 0 }
+    private fun BooleanArray.sumOfPotNumbers(generations: Int) =
+        mapIndexed { index, b -> if (b) (index - (generations * 2)) else 0 }
             .sum()
 
     override fun part1(): Int {
@@ -58,7 +53,6 @@ class Y18D12(input: String) : Day {
         // Thus, the strategy is to look at generations 10 at a time. When the difference between each is the 
         // same, we can surmise that the growth has stabilized. That group provides enough information to 
         // solve part 2.
-
         val groupSize = 10
         val firstStable: List<IndexedValue<Int>> = generator
             .withIndex() // pair up rows with their index, which is the number of generations
