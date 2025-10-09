61ab0857d887549bb6145043ad143a30fd226c10
nbanman <neil.banman@gmail.com>
2025-10-09 14:56:46 -0700

    find factors more efficiently

--- kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2018/Y18D19.kt
+++ kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2018/Y18D19.kt
@@ -1,27 +1,26 @@
 package org.gristle.pdxpuzzles.advent.y2018
 
 import org.gristle.pdxpuzzles.advent.utilities.Day
+import kotlin.math.sqrt
 
 data class Command(val op: Ops, val p: Int, val a: Int, val b: Int, val c: Int) {
     fun execute(reg: LongArray) = op.fn(reg, p, a, b, c)
 }
 
 class Y18D19(input: String) : Day {
-    private val data = input.lines()
-
+    private val p = input.lineSequence().first().takeLast(1).toInt()
+    private val commands = input
+        .lineSequence()
+        .drop(1)
+        .map { line ->
+            line
+                .split(" ")
+                .let { fields ->
+                    val ops = Ops.from(fields[0])
+                    Command(ops, p, fields[1].toInt(), fields[2].toInt(), fields[3].toInt())
+                }
+        }.toList()
     override fun part1(): Long {
-        val p = data.first().takeLast(1).toInt()
-        val commands = data
-            .drop(1)
-            .map { line ->
-                line
-                    .split(" ")
-                    .let { fields ->
-                        val ops = Ops.from(fields[0])
-                        Command(ops, p, fields[1].toInt(), fields[2].toInt(), fields[3].toInt())
-                    }
-            }
-
         val register = LongArray(6)
         while (register[p] in commands.indices) {
             commands[register[p].toInt()].execute(register)
@@ -30,14 +29,32 @@ class Y18D19(input: String) : Day {
     }
 
     override fun part2(): Int {
-
-        // Loop is such that R3 starts as 1, R5 goes up by 1. R2 is R3 * R5. When R2 equals 105k, R0+= R3 and R3++, R5 resets
-        // If R2 goes past 105K w/o equaling it (not divisible), then R3++ and R5 resets w/o RO going up. Thus, RO adds all
-        // the numbers that divide evenly into 105k.
-        val targetNum = 10_551_370
-        return (1..targetNum).fold(0) { acc, i ->
-            if (targetNum % i == 0) acc + i else acc
+        val register = LongArray(6)
+        var prev = register[p]
+        register[0] = 1
+        while (true) {
+            commands[register[p].toInt()].execute(register)
+            if (register[p] >= prev) {
+                prev = register[p]
+            } else {
+                break
+            }
         }
+
+        val c = commands[20].c
+        val targetNum = register[c].toInt()
+
+        // Loop is such that R3 starts as 1, R5 goes up by 1. R2 is R3 * R5. When R2 equals 10.5M, R0+= R3
+        // and R3++, R5 resets. If R2 goes past 10.5M w/o equaling it (not divisible), then R3++ and R5 resets
+        // w/o RO going up. Thus, RO adds all the numbers that divide evenly into 10.5M. So add up all the
+        // factors of that.
+        var factorSum = targetNum + 1
+        for (i in 2..sqrt(targetNum.toDouble()).toInt()) {
+            if (targetNum % i == 0) {
+                factorSum += i + targetNum / i
+            }
+        }
+        return factorSum
     }
 }
 
