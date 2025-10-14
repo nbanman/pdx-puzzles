812e5df40dfd884c3a3c4cf67f22ab6eb200b9b9
nbanman <neil.banman@gmail.com>
2025-10-14 08:56:39 -0700

    replaced opaque recursive region finder with basic DFS

--- kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2017/Y17D14.kt
+++ kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2017/Y17D14.kt
@@ -5,23 +5,6 @@ import org.gristle.pdxpuzzles.advent.y2017.shared.denseHash
 import org.gristle.pdxpuzzles.utilities.objects.toGrid
 
 class Y17D14(input: String) : Day {
-
-    private fun allLinks(links: Map<Int, List<Int>>, seed: Int): List<Int> {
-
-        tailrec fun aL(found: MutableSet<Int>, evaluate: Set<Int>): Set<Int> {
-            return if (evaluate.isEmpty()) {
-                found
-            } else {
-                found.addAll(evaluate)
-                val newRegisters = evaluate.fold(mutableListOf<Int>()) { acc, i ->
-                    acc.apply { addAll(links.getValue(i).filter { it !in found }) }
-                }.toSet()
-                aL(found, newRegisters)
-            }
-        }
-        return aL(mutableSetOf(), setOf(seed)).toList()
-    }
-
     private fun stringRep(input: String): String {
         val preparation = input
             .map { it.code } + listOf(17, 31, 73, 47, 23)
@@ -34,46 +17,50 @@ class Y17D14(input: String) : Day {
                 .let { Integer.toBinaryString(it) }
         }
 
-        return buildString {
+        val ret = buildString {
             binary.forEach {
                 val leadingZeros = "0".repeat(4 - it.length)
                 append(leadingZeros + it)
             }
         }
+        return ret
     }
 
     private val rows = List(128) { i ->
         stringRep("$input-$i")
     }
 
-    override fun part1() = rows.sumOf { row -> row.count { it == '1' } }
+    override fun part1() = rows.sumOf { row ->
+        row.count { it == '1' }
+    }
 
     override fun part2(): Int {
-        val width = rows.first().length
-        val grid = List(width * rows.size) { index ->
-            val x = index % width
-            val y = index / width
-            rows[y][x]
-        }.toGrid(width)
+        val grid = rows.joinToString("\n").toGrid { it == '1' }
+        var regions = 0
+        val visited = BooleanArray(grid.size)
+        val queue: MutableList<Int> = ArrayList(300)
 
-        val registers = mutableMapOf<Int, List<Int>>()
-
-        grid.forEachIndexed { index, c ->
-            if (c == '1') {
-                registers[index] = grid.getNeighborIndices(index).filter { grid[it] == '1' }
+        for ((pos, _) in grid.withIndex().filter { it.value }) {
+            if (visited[pos]) continue
+            regions++
+            queue.add(pos)
+            while (queue.isNotEmpty()) {
+                val current = queue.removeLast()
+                for ((nPos, used) in grid.getNeighborsIndexedValue(current)) {
+                    if (used && !visited[nPos]) {
+                        visited[nPos] = true
+                        queue.add(nPos)
+                    }
+                }
             }
         }
-
-        val linkSet = registers.keys.toMutableSet()
-
-        return generateSequence(linkSet) { it.apply { removeAll(allLinks(registers, linkSet.first()).toSet()) } }
-            .indexOfFirst { it.isEmpty() }
+        return regions
     }
 }
 
 fun main() = Day.runDay(Y17D14::class)
 
-//    Class creation: 442ms
-//    Part 1: 8222 (1ms)
-//    Part 2: 1086 (105ms)
-//    Total time: 549ms
+//    Class creation: 298ms
+//    Part 1: 8222 (3ms)
+//    Part 2: 1086 (21ms)
+//    Total time: 322ms
