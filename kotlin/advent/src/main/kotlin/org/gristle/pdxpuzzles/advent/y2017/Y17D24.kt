cfff926a7884d4a6077dd8269073fadb9e2ae98a
nbanman <neil.banman@gmail.com>
2025-10-15 22:23:10 -0700

    2x speedup

--- kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2017/Y17D24.kt
+++ kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2017/Y17D24.kt
@@ -3,44 +3,60 @@ package org.gristle.pdxpuzzles.advent.y2017
 import org.gristle.pdxpuzzles.advent.utilities.Day
 import org.gristle.pdxpuzzles.utilities.parsing.getInts
 
-private typealias Bridge = List<Y17D24.MagComp>
-
 class Y17D24(input: String) : Day {
 
-    data class MagComp(val a: Int = 0, val b: Int = 0) {
+    data class Stats(val length: Int, val strength: Int)
+    data class Component(val id: Int, val a: Int, val b: Int) {
         val strength = a + b
-        fun canJoin(n: Int) = n == a || n == b
-        fun otherEnd(n: Int) = if (n == a) b else a
+        fun otherEnd(n: Int): Int = if (n == a) b else a
+    }
+    data class State(val n: Int, val strength: Int, val visited: Long) {
+        val len = visited.countOneBits()
     }
 
-    private fun Bridge.strength() = sumOf(MagComp::strength)
-
-    private fun buildBridge(
-        comparator: Comparator<Bridge>,
-        n: Int = 0,
-        bridge: Bridge = listOf(),
-        remaining: Bridge = components,
-    ): Bridge {
-        return remaining
-            .filter { it.canJoin(n) }
-            .map { buildBridge(comparator, it.otherEnd(n), bridge + it, remaining - it) }
-            .maxWithOrNull(comparator) ?: bridge
+    private val parts: Map<Int, List<Component>> = buildMap<Int, MutableList<Component>> {
+        for (component in input
+            .getInts()
+            .chunked(2)
+            .mapIndexed { id, (a, b) ->  Component(id, a, b) }
+        ) {
+            getOrPut(component.a) { mutableListOf() }.add(component)
+            getOrPut(component.b) { mutableListOf() }.add(component)
+        }
     }
 
-    private val components = input
-        .getInts()
-        .chunked(2) { (a, b) -> MagComp(a, b) }
-        .toList()
+    fun buildBridge(parts: Map<Int, List<Component>>, comparator: Comparator<Stats>): Int {
+        val q = mutableListOf(State(0, 0, 0))
+        var maxStats = Stats(0, 0)
+        while (q.isNotEmpty()) {
+            val cur = q.removeLast()
+            var candidateCount = 0
+            val candidates = parts.getValue(cur.n)
+                .asSequence()
+                .filter { (id) -> (cur.visited shr id) and 1L == 0L }
+            for (component in candidates) {
+                candidateCount++
+                val strength = cur.strength + component.strength
+                val n = component.otherEnd(cur.n)
+                val visited = cur.visited + (1L shl component.id)
+                q.add(State(n, strength, visited))
+            }
 
-    private val compareByStrength = compareBy { bridge: Bridge -> bridge.strength() }
+            if (candidateCount == 0) {
+                val stats = Stats(cur.len, cur.strength)
+                if (comparator.compare(maxStats, stats) == -1) maxStats = stats
+            }
+        }
+        return maxStats.strength
+    }
 
-    override fun part1() = buildBridge(compareByStrength).strength()
-    override fun part2() = buildBridge(compareBy(Bridge::size) then compareByStrength).strength()
+    override fun part1() = buildBridge(parts, compareBy(Stats::strength))
+    override fun part2() = buildBridge(parts, compareBy(Stats::length) then compareBy(Stats::strength))
 }
 
-fun main() = Day.runDay(Y17D24::class)
+fun main() = Day.benchmarkDay(Y17D24::class)
 
-//    Class creation: 2ms
-//    Part 1: 1868 (378ms)
-//    Part 2: 1841 (298ms)
-//    Total time: 680ms
+//    Class creation: 4ms
+//    Part 1: 1868 (191ms)
+//    Part 2: 1841 (96ms)
+//    Total time: 292ms
