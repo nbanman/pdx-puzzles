243efc184c0848f1a9346681aa71d0555031ee11
nbanman <neil.banman@gmail.com>
2025-10-18 08:37:50 -0700

    replace Grid wit &Grid

--- rust/utilities/src/structs/grid/grid_scannable.rs
+++ rust/utilities/src/structs/grid/grid_scannable.rs
@@ -1,18 +1,6 @@
 use advent_ocr::Scannable;
 use crate::structs::grid::Grid;
 
-impl Scannable for Grid<bool, 2> {
-    fn normalize(&self) -> String {
-        let mut image = String::new();
-        for row in self.rows() {
-            let row_iter = row.iter().map(|&&on| if on { '#' } else { '.' });
-            image.extend(row_iter);
-            image.push('\n');
-        }
-        image
-    }
-}
-
 impl Scannable for &Grid<bool, 2> {
     fn normalize(&self) -> String {
         let mut image = String::new();
@@ -25,7 +13,7 @@ impl Scannable for &Grid<bool, 2> {
     }
 }
 
-impl Scannable for Grid<char, 2> {
+impl Scannable for &Grid<char, 2> {
     fn normalize(&self) -> String {
         let mut image = String::new();
         for row in self.rows() {
