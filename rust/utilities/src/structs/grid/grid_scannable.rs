1413effdb7e58db4eb7cc3c576ae60287e2bf5bd
nbanman <neil.banman@gmail.com>
2025-10-18 08:33:04 -0700

    added &Grid Scannable

--- rust/utilities/src/structs/grid/grid_scannable.rs
+++ rust/utilities/src/structs/grid/grid_scannable.rs
@@ -13,6 +13,18 @@ impl Scannable for Grid<bool, 2> {
     }
 }
 
+impl Scannable for &Grid<bool, 2> {
+    fn normalize(&self) -> String {
+        let mut image = String::new();
+        for row in self.rows() {
+            let row_iter = row.iter().map(|&&on| if on { '#' } else { '.' });
+            image.extend(row_iter);
+            image.push('\n');
+        }
+        image
+    }
+}
+
 impl Scannable for Grid<char, 2> {
     fn normalize(&self) -> String {
         let mut image = String::new();
