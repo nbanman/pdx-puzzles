66fe2663fdce19de0f42cbaca817c94db121327e
nbanman <neil.banman@gmail.com>
2025-10-21 22:44:15 -0700

    2x speed using Vec<bool> instead of u128

--- rust/advent/src/bin/2016/y16d18.rs
+++ rust/advent/src/bin/2016/y16d18.rs
@@ -3,8 +3,8 @@ use std::iter::successors;
 use advent::utilities::get_input::get_input;
 use utilities::structs::stopwatch::{ReportDuration, Stopwatch};
 
-type Row = u128;
-type Output = u32;
+type Row = Vec<bool>;
+type Output = usize;
 
 fn main() {
     let mut stopwatch = Stopwatch::new();
@@ -18,35 +18,33 @@ fn main() {
 }
 
 // store the row as a u128, add a dummy 
-fn parse_input(input: &str) -> (Row, usize) {
-    let row = input.chars().fold(0u128, |acc, c| (acc << 1) + if c == '.' { 1 } else { 0 });
-    (row, input.len())
+fn parse_input(input: &str) -> Row {
+    input.chars().map(|c| c == '.').collect()
 }
 
-fn next(row: Row, len: usize) -> Row {
-    (0..len).fold(0, |acc, i| {
-        let n = match i {
-            0 => (row >> 1) & 1,
-            i if i == len - 1 => (row >> (len - 2)) & 1,
-            i => if ((row >> (i + 1)) & 1) == ((row >> (i - 1)) & 1) { 1 } else { 0 },
-        };
-        (acc << 1) + n
-    })
+fn next(row: &Row) -> Row {
+    (0..row.len()).map(|i| {
+        match i {
+            0 => row[1],
+            i if i == row.len() - 1 => row[i - 1],
+            i => row[i - 1] == row[i + 1],
+        }
+    }).collect()
 }
 
-fn solve(row: Row, len: usize, rows: usize) -> Output {
-    successors(Some(row), |acc| Some(next(*acc, len)))
+fn solve(row: Row, rows: usize) -> Output {
+    successors(Some(row), |acc| Some(next(acc)))
         .take(rows)
-        .map(|row| row.count_ones())
+        .map(|row| row.iter().filter(|&&x| x).count())
         .sum()
 }
 
-fn part1((row, len): (Row, usize)) -> Output {
-    solve(row, len, 40)
+fn part1(row: Row) -> Output {
+    solve(row, 40)
 }
 
-fn part2((row, len): (Row, usize)) -> Output {
-    solve(row, len, 400_000)
+fn part2(row: Row) -> Output {
+    solve(row, 400_000)
 }
 
 #[test]
@@ -57,7 +55,7 @@ fn default() {
     assert_eq!(19984714, part2(input));
 }
 
-// Input parsed (15μs)
-// 1. 1987 (16μs)
-// 2. 19984714 (79ms)
-// Total: 79ms
+// Input parsed (16μs)
+// 1. 1987 (9μs)
+// 2. 19984714 (31ms)
+// Total: 31ms
