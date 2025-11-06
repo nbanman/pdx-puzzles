8f8aba0038af767cd99b5dcb84911bf1636fe783
nbanman <neil.banman@gmail.com>
2025-11-06 08:33:16 -0800

    remove sort from part1 since .distinct() relies on hashing to dedup

--- kotlin/everybody_codes/src/main/kotlin/org/gristle/pdxpuzzles/everybodycodes/y2025/Y25D03.kt
+++ kotlin/everybody_codes/src/main/kotlin/org/gristle/pdxpuzzles/everybodycodes/y2025/Y25D03.kt
@@ -5,7 +5,7 @@ import org.gristle.pdxpuzzles.utilities.parsing.getIntList
 import org.gristle.pdxpuzzles.utilities.parsing.getInts
 
 object Y25D03 : Day {
-    override fun part1(input: String): Int = input.getInts().sorted().distinct().sum()
+    override fun part1(input: String): Int = input.getInts().distinct().sum()
     override fun part2(input: String): Int = input.getInts().sorted().distinct().take(20).sum()
     override fun part3(input: String): Int = input.getIntList().groupingBy { it }.eachCount().values.max()
 }
