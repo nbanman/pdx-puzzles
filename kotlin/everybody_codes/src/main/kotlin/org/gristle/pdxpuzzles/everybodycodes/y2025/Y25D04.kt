16c02a8c7a64e4fb4d387cf623ed0a4cded525eb
nbanman <neil.banman@gmail.com>
2025-11-06 16:30:46 -0800

    initial

---
+++ kotlin/everybody_codes/src/main/kotlin/org/gristle/pdxpuzzles/everybodycodes/y2025/Y25D04.kt
@@ -0,0 +1,34 @@
+package org.gristle.pdxpuzzles.everybodycodes.y2025
+
+import org.gristle.pdxpuzzles.everybodycodes.utilities.Day
+import org.gristle.pdxpuzzles.utilities.math.isEven
+import org.gristle.pdxpuzzles.utilities.parsing.getInts
+import kotlin.math.ceil
+
+object Y25D04 : Day {
+    private fun firstAndLast(input: String): Pair<Double, Double> {
+        val first = input.takeWhile { it.isDigit() }.toDouble()
+        val last = input.takeLastWhile { it.isDigit() }.toDouble()
+        return first to last
+    }
+
+    override fun part1(input: String) = firstAndLast(input)
+        .let { (first, last) -> (2025.0 * (first / last)).toLong() }
+
+    override fun part2(input: String) = firstAndLast(input)
+        .let { (first, last) -> ceil(10_000_000_000_000.0 * (last / first)).toLong() }
+
+    override fun part3(input: String) = input
+        .getInts()
+        .zipWithNext()
+        .filterIndexed { index, _-> index.isEven() }
+        .fold(100.0) { acc, (a, b) -> acc * (a.toDouble() / b)}
+        .toLong()
+}
+
+fun main() = Day.runDay(Y25D04::class)
+
+//    Quest 1: 12980 (0ms)
+//    Quest 2: 2394789579159 (2ms)
+//    Quest 3: 220503433846 (2ms)
+//    Total time: 5ms
---
+++ rust/everybody-codes/src/bin/2025/y25d04.rs
@@ -0,0 +1,58 @@
+extern crate core;
+
+use everybody_codes::utilities::inputs::get_event_inputs;
+use itertools::Itertools;
+use utilities::{parsing::get_numbers::ContainsNumbers, structs::stopwatch::{ReportDuration, Stopwatch}};
+
+type Input<'a> = &'a str;
+
+fn main() {
+    let mut stopwatch = Stopwatch::new();
+    stopwatch.start();
+    let (input1, input2, input3) = get_event_inputs(25, 4);
+    println!("Input parsed ({})", stopwatch.lap().report());
+    println!("1. {} ({})", part1(&input1), stopwatch.lap().report());
+    println!("2. {} ({})", part2(&input2), stopwatch.lap().report());
+    println!("3. {} ({})", part3(&input3), stopwatch.lap().report());
+    println!("Total: {}", stopwatch.stop().report());
+}
+
+fn first_and_last(input: Input) -> (f64, f64) {
+    let first = input.as_bytes().iter().position(|it| !it.is_ascii_digit()).unwrap();
+    let first = input[0..first].parse().unwrap();
+    let last = input.trim_end().as_bytes().iter().rposition(|it| !it.is_ascii_digit()).unwrap();
+    let last = input.trim_end()[last + 1..].parse().unwrap();
+    (first, last)
+}
+
+fn part1(input: Input) -> u64 {
+    let (first, last) = first_and_last(input);
+    (2025.0 * (first / last)) as u64
+}
+
+fn part2(input: Input) -> u64 {
+    let (first, last) = first_and_last(input);
+    (10_000_000_000_000.0 * (last / first)).ceil() as u64
+}
+
+fn part3(input: Input) -> u64 {
+    input.get_numbers::<usize>()
+        .tuple_windows()
+        .enumerate()
+        .filter(|(index, _)| index & 1 == 0)
+        .fold(100f64, |acc, (_, (a, b))| acc * (a as f64 / b as f64)) as u64
+}
+
+#[test]
+fn default() {
+    let (input1, input2, input3) = get_event_inputs(25, 4);
+    assert_eq!(12980, part1(&input1));
+    assert_eq!(2394789579159, part2(&input2));
+    assert_eq!(220503433846, part3(&input3));
+}
+
+// Input parsed (28μs)
+// 1. 12980 (8μs)
+// 2. 2394789579159 (2μs)
+// 3. 220503433846 (5μs)
+// Total: 46μs
