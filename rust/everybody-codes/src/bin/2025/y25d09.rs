ccf675e33e976bc0ef42e08f9cda354447e83044
nbanman <neil.banman@gmail.com>
2025-11-15 15:30:33 -0800

    formatting

--- rust/everybody-codes/src/bin/2025/y25d09.rs
+++ rust/everybody-codes/src/bin/2025/y25d09.rs
@@ -5,9 +5,9 @@ use core::simd::u64x4;
 #[cfg(feature = "simd")]
 use std::simd::num::SimdUint;
 
-use std::ops::{BitAnd, BitOr};
 use everybody_codes::utilities::inputs::get_event_inputs;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use std::ops::{BitAnd, BitOr};
 use utilities::structs::stopwatch::{ReportDuration, Stopwatch};
 
 type Input<'a> = &'a str;
@@ -21,26 +21,28 @@ struct Dna([u64; 8]);
 
 impl Dna {
     fn array(value: &str) -> [u64; 8] {
+        let (_, value) = value.split_once(':').unwrap();
         let bytes = value.as_bytes();
         std::array::from_fn(|i| {
             let bytes = &bytes[i * 16..i * 16 + 16];
             bytes.iter().fold(0u64, |acc, &b| {
-                (acc << 4) | match b {
-                    b'A' => 1,
-                    b'T' => 2,
-                    b'C' => 4,
-                    b'G' => 8,
-                    b => panic!("{} not a valid symbol", b as char),
-                }
+                (acc << 4)
+                    | match b {
+                        b'A' => 1,
+                        b'T' => 2,
+                        b'C' => 4,
+                        b'G' => 8,
+                        b => panic!("{} not a valid symbol", b as char),
+                    }
             })
         })
     }
 
-
     #[cfg(feature = "simd")]
     fn similarity(&self, other: &Dna) -> usize {
         let meld = *self & *other;
-        meld.0.into_iter()
+        meld.0
+            .into_iter()
             .map(|it| it.count_ones().reduce_sum() as usize)
             .sum()
     }
@@ -48,9 +50,7 @@ impl Dna {
     #[cfg(not(feature = "simd"))]
     fn similarity(&self, other: &Dna) -> usize {
         let meld = *self & *other;
-        meld.0.into_iter()
-            .map(|it| it.count_ones() as usize)
-            .sum()
+        meld.0.into_iter().map(|it| it.count_ones() as usize).sum()
     }
 }
 
@@ -59,20 +59,15 @@ impl BitAnd for Dna {
 
     #[cfg(feature = "simd")]
     fn bitand(self, rhs: Self) -> Self::Output {
-        let inner: [u64x4; 2] = std::array::from_fn(|i| {
-            self.0[i] & rhs.0[i]
-        });
+        let inner: [u64x4; 2] = std::array::from_fn(|i| self.0[i] & rhs.0[i]);
         Self(inner)
     }
 
     #[cfg(not(feature = "simd"))]
     fn bitand(self, rhs: Self) -> Self::Output {
-        let inner: [u64; 8] = std::array::from_fn(|i| {
-            self.0[i] & rhs.0[i]
-        });
+        let inner: [u64; 8] = std::array::from_fn(|i| self.0[i] & rhs.0[i]);
         Self(inner)
     }
-
 }
 
 impl BitOr for Dna {
@@ -80,57 +75,28 @@ impl BitOr for Dna {
 
     #[cfg(feature = "simd")]
     fn bitor(self, rhs: Self) -> Self::Output {
-        let inner: [u64x4; 2] = std::array::from_fn(|i| {
-            self.0[i] | rhs.0[i]
-        });
+        let inner: [u64x4; 2] = std::array::from_fn(|i| self.0[i] | rhs.0[i]);
         Self(inner)
     }
 
     #[cfg(not(feature = "simd"))]
     fn bitor(self, rhs: Self) -> Self::Output {
-        let inner: [u64; 8] = std::array::from_fn(|i| {
-            self.0[i] | rhs.0[i]
-        });
+        let inner: [u64; 8] = std::array::from_fn(|i| self.0[i] | rhs.0[i]);
         Self(inner)
     }
-
 }
 
 impl From<&str> for Dna {
     #[cfg(feature = "simd")]
     fn from(value: &str) -> Self {
         let arr = Self::array(value);
-        Dna([
-            u64x4::from_slice(&arr[..4]),
-            u64x4::from_slice(&arr[4..]),
-        ])
+        Dna([u64x4::from_slice(&arr[..4]), u64x4::from_slice(&arr[4..])])
     }
 
     #[cfg(not(feature = "simd"))]
     fn from(value: &str) -> Self {
         Dna(Self::array(value))
     }
-}
-
-fn main() {
-    let mut stopwatch = Stopwatch::new();
-    stopwatch.start();
-    let (input1, input2, input3) = get_event_inputs(25, 9);
-    println!("Input parsed ({})", stopwatch.lap().report());
-    println!("1. {} ({})", part1(&input1), stopwatch.lap().report());
-    println!("2. {} ({})", part2(&input2), stopwatch.lap().report());
-    println!("3. {} ({})", part3(&input3), stopwatch.lap().report());
-    println!("Total: {}", stopwatch.stop().report());
-}
-
-fn get_dna(input: Input) -> Vec<Dna> {
-    input
-        .lines()
-        .map(|line| {
-            let (_, dna) = line.split_once(':').unwrap();
-            dna.into()
-        })
-        .collect()
 }
 
 struct UnionFind {
@@ -179,6 +145,21 @@ impl UnionFind {
     }
 }
 
+fn main() {
+    let mut stopwatch = Stopwatch::new();
+    stopwatch.start();
+    let (input1, input2, input3) = get_event_inputs(25, 9);
+    println!("Input parsed ({})", stopwatch.lap().report());
+    println!("1. {} ({})", part1(&input1), stopwatch.lap().report());
+    println!("2. {} ({})", part2(&input2), stopwatch.lap().report());
+    println!("3. {} ({})", part3(&input3), stopwatch.lap().report());
+    println!("Total: {}", stopwatch.stop().report());
+}
+
+fn get_dna(input: Input) -> Vec<Dna> {
+    input.lines().map(Dna::from).collect()
+}
+
 fn get_families(dna: &[Dna], child: usize, child_dna: &Dna) -> Option<(usize, usize)> {
     (0..dna.len())
         .filter(|&it| it != child)
@@ -224,10 +205,7 @@ fn part2(input: Input) -> usize {
         .filter_map(|child| {
             let child_dna = dna[child];
             get_families(&dna, child, &child_dna)
-                .map(|(p1, p2)| {
-                    dna[child].similarity(&dna[p1])
-                        * dna[child].similarity(&dna[p2])
-                })
+                .map(|(p1, p2)| dna[child].similarity(&dna[p1]) * dna[child].similarity(&dna[p2]))
         })
         .sum()
 }
@@ -244,19 +222,23 @@ fn part3(input: Input) -> usize {
 
     let mut tree = UnionFind::new(500);
     for (a, b, c) in nuclear_families {
-        // println!("({a}, {b}, {c})");
         tree.union(a, b);
         tree.union(a, c);
     }
 
-    let largest_group = tree.size.iter().enumerate()
+    let largest_group = tree
+        .size
+        .iter()
+        .enumerate()
         .max_by_key(|&(_, len)| len)
         .map(|(index, _)| index)
         .unwrap();
 
     tree.update();
 
-    tree.parent.iter().enumerate()
+    tree.parent
+        .iter()
+        .enumerate()
         .filter(|&(_, &parent)| parent == largest_group)
         .map(|(index, _)| index + 1)
         .sum()
