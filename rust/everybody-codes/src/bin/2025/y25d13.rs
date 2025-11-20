c8c029d984a3fc340e02b1467f536b6ec009ec06
nbanman <neil.banman@gmail.com>
2025-11-19 19:14:08 -0800

    simplify and add comments

--- rust/everybody-codes/src/bin/2025/y25d13.rs
+++ rust/everybody-codes/src/bin/2025/y25d13.rs
@@ -1,5 +1,4 @@
-use std::{collections::VecDeque, ops::RangeInclusive};
-
+use std::collections::VecDeque;
 use everybody_codes::utilities::inputs::get_event_inputs;
 use itertools::Itertools;
 use utilities::{parsing::get_numbers::ContainsNumbers, structs::stopwatch::{ReportDuration, Stopwatch}};
@@ -17,52 +16,71 @@ fn main() {
     println!("Total: {}", stopwatch.stop().report());
 }
 
-fn solve(ranges: impl Iterator<Item = RangeInclusive<u64>>, total_turns: u64) -> u64 {
-    let mut lock: VecDeque<(RangeInclusive<u64>, bool)> = VecDeque::with_capacity(500);
-    lock.push_back((1..=1, true));
-    let mut start = 0;
-    let mut total: u64 = 1;
+fn solve(ranges: impl Iterator<Item = (u32, u32)>, total_turns: u64) -> u32 {
+    // ranges stored in VecDeque to approximate 'circular' layout of the ranges
+    let mut lock: VecDeque<(u32, u32)> = VecDeque::with_capacity(500);
+    lock.push_back((1, 1));
 
-    for (idx, rng) in ranges.enumerate() {
-        total += rng.end() - rng.start() + 1;
-        if idx & 1 == 0 {
-            lock.push_back((rng, true));
+    // we don't know how many ranges there are in the lock to start, so we track it as we
+    // iterate. When the left half gets pushed on to the Deque, the index will shift so
+    // this will ensure that we start at the 12 o'clock position once the lock is built.
+    let mut start = 0;
+
+    // The total number of spots on the lock, not the range, but as if the range were exploded.
+    // Starts at 1 to account for the 12 o'clock spot that is not in the input.
+    let mut total = 1;
+
+    // This bool cycles on and off, telling us to place ranges forward or backward on the lock.
+    let mut forward = true;
+
+    for (lo, hi) in ranges {
+        total += hi - lo + 1;
+        if forward {
+            lock.push_back((lo, hi));
         } else {
             start += 1;
-            lock.push_front((rng, false))
-        }      
+            lock.push_front((lo, hi))
+        }
+        forward = !forward;
     }
 
-    let total_turns = (total_turns + 1) % total;
-    let mut turns: u64 = 0;
+    let total_turns = ((total_turns + 1) % total as u64) as u32;
+    let mut turns= 0;
     
     for i in start.. {
-        let (rng, is_forward) = lock.get(i % lock.len()).unwrap();
-        turns += rng.end() - rng.start() + 1;
+        let i = i % lock.len();
+        let &(lo, hi) = lock.get(i).unwrap();
+        turns += hi - lo + 1;
         if turns >= total_turns {
             let diff = turns - total_turns;
-            return if *is_forward {
-                rng.end() - diff
+            return if i >= start {
+                hi - diff
             } else {
-                rng.start() + diff
+                lo + diff
             }
         }
     }
     unreachable!()
 }
 
-fn part1(input: Input) -> u64 {
-    let ranges = input.get_numbers().map(|n| n..=n);
+fn part1(input: Input) -> u32 {
+    let ranges = input
+        .get_numbers()
+        .map(|n| (n, n));
     solve(ranges, 2025)
 }
-
-fn part2(input: Input) -> u64 {
-    let ranges = input.get_numbers().tuples().map(|(a, b)| a..=b);
+fn part2(input: Input) -> u32 {
+    let ranges = input
+        .get_numbers()
+        .tuples()
+        .map(|(a, b)| (a, b));
     solve(ranges, 20_252_025)
 }
-
-fn part3(input: Input) -> u64 {
-    let ranges = input.get_numbers().tuples().map(|(a, b)| a..=b);
+fn part3(input: Input) -> u32 {
+    let ranges = input
+        .get_numbers()
+        .tuples()
+        .map(|(a, b)| (a, b));
     solve(ranges, 202_520_252_025)
 }
 
@@ -75,7 +93,7 @@ fn default() {
 }
 
 // Input parsed (28μs)
-// 1. 353 (8μs)
-// 2. 7613 (6μs)
-// 3. 217823 (20μs)
-// Total: 65μs
+// 1. 353 (10μs)
+// 2. 7613 (4μs)
+// 3. 217823 (14μs)
+// Total: 59μs
