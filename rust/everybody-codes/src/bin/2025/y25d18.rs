a0dbdb64993f809a08def7ac1016a3d93bb51c4f
nbanman <neil.banman@gmail.com>
2025-11-29 09:41:00 -0800

    tweak

--- rust/everybody-codes/src/bin/2025/y25d18.rs
+++ rust/everybody-codes/src/bin/2025/y25d18.rs
@@ -114,7 +114,13 @@ fn part3(notes: Input) -> i64 {
     let last = plants.last().unwrap();
 
     let mut plant_energy: Vec<Option<i64>> = vec![None; plants.len()];
-    for i in 0..81 {
+
+    let phase_2_start = plants.iter()
+        .position(|plant| {
+            matches!(plant.branches, Branches::ToPlants(_))
+        })
+        .unwrap();
+    for i in 0..phase_2_start {
         plant_energy[i] = Some(0);
     }
     
@@ -157,107 +163,6 @@ fn part3(notes: Input) -> i64 {
         .sum()
 }
 
-#[test]
-fn part_one() {
-    let notes = r"Plant 1 with thickness 1:
-- free branch with thickness 1
-
-Plant 2 with thickness 1:
-- free branch with thickness 1
-
-Plant 3 with thickness 1:
-- free branch with thickness 1
-
-Plant 4 with thickness 17:
-- branch to Plant 1 with thickness 15
-- branch to Plant 2 with thickness 3
-
-Plant 5 with thickness 24:
-- branch to Plant 2 with thickness 11
-- branch to Plant 3 with thickness 13
-
-Plant 6 with thickness 15:
-- branch to Plant 3 with thickness 14
-
-Plant 7 with thickness 10:
-- branch to Plant 4 with thickness 15
-- branch to Plant 5 with thickness 21
-- branch to Plant 6 with thickness 34";
-
-    assert_eq!(774, part1(notes));
-}
-
-#[test]
-fn part_two() {
-    let notes = r"Plant 1 with thickness 1:
-- free branch with thickness 1
-
-Plant 2 with thickness 1:
-- free branch with thickness 1
-
-Plant 3 with thickness 1:
-- free branch with thickness 1
-
-Plant 4 with thickness 10:
-- branch to Plant 1 with thickness -25
-- branch to Plant 2 with thickness 17
-- branch to Plant 3 with thickness 12
-
-Plant 5 with thickness 14:
-- branch to Plant 1 with thickness 14
-- branch to Plant 2 with thickness -26
-- branch to Plant 3 with thickness 15
-
-Plant 6 with thickness 150:
-- branch to Plant 4 with thickness 5
-- branch to Plant 5 with thickness 6
-
-
-1 0 1
-0 0 1
-0 1 1";
-
-    assert_eq!(324, part2(notes));
-}
-
-#[test]
-fn part_three() {
-    let notes = r"Plant 1 with thickness 1:
-- free branch with thickness 1
-
-Plant 2 with thickness 1:
-- free branch with thickness 1
-
-Plant 3 with thickness 1:
-- free branch with thickness 1
-
-Plant 4 with thickness 1:
-- free branch with thickness 1
-
-Plant 5 with thickness 8:
-- branch to Plant 1 with thickness -8
-- branch to Plant 2 with thickness 11
-- branch to Plant 3 with thickness 13
-- branch to Plant 4 with thickness -7
-
-Plant 6 with thickness 7:
-- branch to Plant 1 with thickness 14
-- branch to Plant 2 with thickness -9
-- branch to Plant 3 with thickness 12
-- branch to Plant 4 with thickness 9
-
-Plant 7 with thickness 23:
-- branch to Plant 5 with thickness 17
-- branch to Plant 6 with thickness 18
-
-
-0 1 0 0
-0 1 0 1
-0 1 1 1
-1 1 0 1";
-
-    assert_eq!(946, part3(notes));
-}
 #[test]
 fn default() {
     // let (input1, input2, input3) = get_event_inputs(25, 18);
