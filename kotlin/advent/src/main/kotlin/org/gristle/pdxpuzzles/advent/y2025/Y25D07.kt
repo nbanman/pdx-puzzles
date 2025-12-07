899fb186d1805eb16970345d16ec57b212ab03b3
nbanman <neil.banman@gmail.com>
2025-12-07 00:56:19 -0800

    used String instead of converting to Grid

--- kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2025/Y25D07.kt
+++ kotlin/advent/src/main/kotlin/org/gristle/pdxpuzzles/advent/y2025/Y25D07.kt
@@ -1,22 +1,21 @@
 package org.gristle.pdxpuzzles.advent.y2025
 
 import org.gristle.pdxpuzzles.advent.utilities.Day
-import org.gristle.pdxpuzzles.utilities.objects.toGrid
 
-class Y25D07(input: String) : Day {
+class Y25D07(manifold: String) : Day {
     private val p1Answer: Int
     private val p2Answer: Long
 
     init {
-        val manifold = input.toGrid()
-        val finished = manifold.size - manifold.width * 2
+        val width = manifold.indexOf('\n') + 1
+        val finished = manifold.length - width * 2 + 1
 
         var splits = 0
 
-        var todo = LongArray(manifold.width)
+        var todo = LongArray(width - 1)
         todo[manifold.indexOf('S')] = 1
-        var next = LongArray(manifold.width)
-        for (row in 0 until manifold.size step manifold.width) {
+        var next = LongArray(width - 1)
+        for (row in 0 until manifold.length step width) {
             for ((pos, timeline) in todo.withIndex()) {
                 if (timeline == 0L) continue
                 if (manifold[pos + row] == '^') {
@@ -30,7 +29,7 @@ class Y25D07(input: String) : Day {
             }
             if (row == finished) break
             todo = next
-            next = LongArray(manifold.width)
+            next = LongArray(width - 1)
         }
         p1Answer = splits
         p2Answer = next.sum()
@@ -41,12 +40,12 @@ class Y25D07(input: String) : Day {
     override fun part2() = p2Answer
 }
 
-fun main() = Day.runDay(Y25D07::class)
+fun main() = Day.benchmarkDay(Y25D07::class)
 
-//    Class creation: 7ms
+//    Class creation: 2ms
 //    Part 1: 1533 (1ms)
 //    Part 2: 10733529153890 (2ms)
-//    Total time: 11ms
+//    Total time: 5ms
 
 @Suppress("unused")
 private val test = listOf(""".......S.......
