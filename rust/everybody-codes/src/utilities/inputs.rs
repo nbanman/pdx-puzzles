e02ff7e94bcaaf66f6eb67d674e348aa7375e882
nbanman <neil.banman@gmail.com>
2026-09-23 15:43:04 -0700

    add provisional input getters

--- rust/everybody-codes/src/utilities/inputs.rs
+++ rust/everybody-codes/src/utilities/inputs.rs
@@ -49,3 +49,26 @@ pub fn get_story_input(year: u16, story: u8, day: u8, quest: u8) -> String {
     file.read_to_string(&mut contents).unwrap();
     contents
 }
+
+pub fn get_story_inputs_provisional(year: u16, story: u8, day: u8) 
+    -> (Option<String>, Option<String>, Option<String>) {
+    let year = if year > 2000 { year - 2000 } else { year };
+    (1..=3)
+        .map(|part| get_story_input_provisional(year, story, day, part))
+        .collect_tuple()
+        .unwrap()
+}
+
+pub fn get_story_input_provisional(year: u16, story: u8, day: u8, quest: u8) -> Option<String> {
+    let manifest_dir =
+        std::env::var("CARGO_MANIFEST_DIR").expect("Failed to find manifest directory");
+    let path = manifest_dir
+        + &format!(
+        "/../../inputs/everybody_codes/20{}/y{}s{}d{:02}q{}.txt",
+        year, year, story, day, quest
+    );
+    let mut file = File::open(&path).ok()?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    Some(contents)
+}
